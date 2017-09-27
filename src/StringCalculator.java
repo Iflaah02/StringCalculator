b823de49b055db9f80a5cbe6c5f31fa3bb276767
keios <keios@10.20.192.144>
2017-09-27 16:19:24 +0300

    assignment finished

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,9 +1,38 @@
 
 public class StringCalculator {
-	public int add(String numbersStr) {
+	private int sum = 0;
+	private String[] arrString;
+	public int add(String numbersStr) throws StringCalculatorException{
 		// Returns the sum of the numbers given in numbersStr
 		
-		// not yet implemented
-		return 0;
+		if (numbersStr.endsWith(",") || numbersStr.endsWith("\n")) throw new StringCalculatorException();
+		splitString(numbersStr);
+	
+		for (String s : this.arrString) {
+			if (!verifyString(s) || s.length() == 0) throw new StringCalculatorException();
+			this.sum += Integer.parseInt(s);
+		}
+		return this.sum;		
 	}
+	
+	public void splitString(String str) {
+		this.arrString = str.split(",|\\n");	
+	}
+	
+	
+	public int getSum() {
+		return this.sum;
+	}
+	
+	public String[] getArrString() {
+		return this.arrString;
+	}
+	
+	public boolean verifyString(String numberStr) {
+		for (char c : numberStr.toCharArray()) {
+			if (!Character.isDigit(c)) return false;
+		}
+		return true;
+	}
+	
 }
--- tests/StringCalculatorTest.java
+++ tests/StringCalculatorTest.java
@@ -5,8 +5,109 @@ import org.junit.Test;
 public class StringCalculatorTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void sumZeroValues() throws StringCalculatorException{
+		StringCalculator calc = new StringCalculator();
+				
+		int sum = calc.getSum();
+		
+		assertEquals("Invalid sum", 0, sum);
+	}
+	@Test
+	public void sumOneValue() throws StringCalculatorException{
+		StringCalculator calc = new StringCalculator();
+		
+		calc.add("1");
+		
+		int sum = calc.getSum();
+		
+		assertEquals("Invalid sum", 1, sum);
+	}
+	@Test
+	public void sumTwoSingleDigitValues() throws StringCalculatorException{
+		StringCalculator calc = new StringCalculator();
+		
+		calc.add("1,2");
+		
+		int sum = calc.getSum();
+		
+		assertEquals("Invalid sum", 3, sum);
+	}
+	
+	@Test
+	public void sumTwoMultipleDigitValues() throws StringCalculatorException{
+		StringCalculator calc = new StringCalculator();
+		
+		calc.add("11,22");
+		
+		int sum = calc.getSum();
+		
+		assertEquals("Invalid sum", 33, sum);
 	}
 
+	@Test
+	public void testSplitString() throws StringCalculatorException{
+		StringCalculator calc = new StringCalculator();
+		
+		calc.splitString("");
+				
+		assertEquals("Invalid splitted string length", 1, calc.getArrString().length);
+	}
+	
+	@Test
+	public void sumNewLine() throws StringCalculatorException{
+		StringCalculator calc = new StringCalculator();
+		
+		calc.add("1\n2");
+		
+		int sum = calc.getSum();
+		
+		assertEquals("Invalid sum", 3, sum);
+	}
+	@Test
+	public void sumNewLineAndComma() throws StringCalculatorException{
+		StringCalculator calc = new StringCalculator();
+		
+		calc.add("1\n2,3");
+		
+		int sum = calc.getSum();
+		
+		assertEquals("Invalid sum", 6, sum);
+	}
+	
+	@Test(expected = StringCalculatorException.class)
+	public void testInvalidString() throws StringCalculatorException{
+		StringCalculator calc = new StringCalculator();
+		
+		calc.add("1,2,asd");
+		
+	}
+	
+	@Test(expected = StringCalculatorException.class)
+	public void testNegativeNumber() throws StringCalculatorException{
+		StringCalculator calc = new StringCalculator();
+		
+		calc.add("-1,3");
+
+	}
+	
+	@Test(expected = StringCalculatorException.class)
+	public void testStringEmptyValueInMiddle() throws StringCalculatorException{
+		StringCalculator calc = new StringCalculator();
+		
+		calc.add("1,3,\n,5");	
+	}
+	
+	@Test(expected = StringCalculatorException.class)
+	public void testStringEndWithNewline() throws StringCalculatorException{
+		StringCalculator calc = new StringCalculator();
+		
+		calc.add("1,3,\n5\n");
+	}
+	
+	@Test(expected = StringCalculatorException.class)
+	public void testStringEndWithComma() throws StringCalculatorException{
+		StringCalculator calc = new StringCalculator();
+		
+		calc.add("1,3,\n5,");
+	}
 }
