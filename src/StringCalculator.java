55d1dff406a871e74c56446dc81bfa099d1a26a1
jmusta <jumustak@gmail.com>
2017-09-27 15:48:56 +0300

    Written method to splits splits the input string and stores it in a String array attribute. Written and ran tests for the setInput method.

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,9 +1,25 @@
 
 public class StringCalculator {
+	
+	protected String[] inputs;
+	
+	StringCalculator(){		
+	}
+	
+	protected void setInput(String input) {
+		inputs = input.split(",");
+	}
+	
+	
+	
 	public int add(String numbersStr) {
 		// Returns the sum of the numbers given in numbersStr
 		
 		// not yet implemented
+		
+		
 		return 0;
 	}
+	
+	
 }
--- tests/StringCalculatorTest.java
+++ tests/StringCalculatorTest.java
@@ -2,11 +2,54 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+
 public class StringCalculatorTest {
 
 	@Test
 	public void test() {
 		fail("Not yet implemented");
 	}
+	
+	@Test
+	public void testSetInput_Empty() {
+		StringCalculator calc = new StringCalculator();
+		
+		calc.setInput("");
+		
+		assertEquals(calc.inputs[0], "");
+	}
+	
+	@Test
+	public void testSetInput_OneNumber() {
+		StringCalculator calc = new StringCalculator();
+		
+		calc.setInput("1");
+		
+		assertEquals(calc.inputs[0], "1");
+	}
+	
+	@Test
+	public void testSetInput_TwoNumbers() {
+		
+		StringCalculator calc = new StringCalculator();
+		calc.setInput("1,2");
+		
+		assertTrue(calc.inputs.length == 2);
+		assertEquals(calc.inputs[0], "1");
+		assertEquals(calc.inputs[1], "2");
+	}
+	
+	@Test
+	public void testSetInput_ThreeNumbers() {
+		StringCalculator calc = new StringCalculator();
+		calc.setInput("1,2,3");
+		
+		assertTrue(calc.inputs.length == 3);
+		assertEquals(calc.inputs[0], "1");
+		assertEquals(calc.inputs[1], "2");
+		assertEquals(calc.inputs[2], "3");
+	}
+	
+	
 
 }
