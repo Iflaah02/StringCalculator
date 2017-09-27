0aa014185db1960362cfb9a935ea8555a7dbcdc3
jekorhon <jekorhon@10.20.208.192>
2017-09-27 15:36:16 +0300

    Implemeting said requests and done unit tests.

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,9 +1,44 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class StringCalculator {
-	public int add(String numbersStr) {
-		// Returns the sum of the numbers given in numbersStr
-		
-		// not yet implemented
-		return 0;
+	public int add(String numbersStr) throws StringCalculatorException {
+		int result = 0;
+		// If numbStr is empty string, return 0
+		if (numbersStr.equals("")) {
+			return 0;
+		}
+		// Parse string
+		List<Integer> numbers = parseString(numbersStr);
+		// Calculate sum
+		for(int num : numbers) {
+			result = result + num;
+		}
+		return result;
+	}
+	
+	public List<Integer> parseString(String numbersStr) throws StringCalculatorException {
+		List<Integer> numbers = new ArrayList<>();
+		// If ends with (, or ,\n) throw exception
+		if (numbersStr.endsWith(",") || numbersStr.endsWith(",\n")) {
+			throw new StringCalculatorException();
+		}
+		// Split string to string parts (new line or comma)
+		for(String token : numbersStr.split(",|\n")) {
+			if (token.isEmpty() || token.endsWith(",") || token.endsWith(",\n")) {
+				throw new StringCalculatorException();
+			}
+			try {
+				int number = Integer.parseInt(token);
+				if (number < 0) {
+					throw new StringCalculatorException();
+				}
+				numbers.add(number);
+			} catch (NumberFormatException e){
+				throw new StringCalculatorException();
+			}
+			
+		}
+		return numbers;
 	}
 }
