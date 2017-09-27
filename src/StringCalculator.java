12b9302c4ee46c4894753a78ed56981b78dc8751
Sgathec <not-a-real-address@fakemail.com>
2017-09-27 17:15:54 +0300

    Assignment finished

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,9 +1,50 @@
 
 public class StringCalculator {
-	public int add(String numbersStr) {
-		// Returns the sum of the numbers given in numbersStr
+	
+	private String string; 
+	
+	public int add(String numbersStr) throws StringCalculatorException {
 		
-		// not yet implemented
-		return 0;
+		string = numbersStr;
+		
+		String[] numbers = splitByCommasOrNewlines(string);
+		
+		int result = 0;
+		
+		for(String number : numbers) {
+			
+			result += validateNumber(number);
+			
+		}
+	
+		return result;
 	}
+	
+	private String[] splitByCommasOrNewlines(String numbers) throws StringCalculatorException {
+		String[] result = numbers.split("[,\\n]");
+		if (numbers.matches(".[\n,][\n,]")) { // Sixth slice implementation (just the if branch)
+			throw new StringCalculatorException("Malformed input");
+		}
+		return result;
+	}
+	
+	private int validateNumber(String number) throws StringCalculatorException {
+		int result;
+		try {
+			result = Integer.valueOf(number);
+			if(result < 0 || number.isEmpty()) {
+				throw new StringCalculatorException("Negative numbers aren't allowed");
+			}
+		}
+		catch(NumberFormatException e) {
+			throw new StringCalculatorException("Can't add something that is not a number");
+		}
+		
+		return result;
+	}
+	
+	public String getString() {
+		return string;
+	}
+	
 }
--- tests/StringCalculatorTest.java
+++ tests/StringCalculatorTest.java
@@ -3,10 +3,98 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class StringCalculatorTest {
-
+	
+	// A test for the seventh slice
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testSettingNumber() throws StringCalculatorException {
+		//Arrange
+		StringCalculator calc = new StringCalculator();
+		String input = "1234";
+		
+		//Act
+		int additionResult = calc.add(input);
+		
+		//Assert
+		assertEquals(input, String.valueOf(calc.getString()));
+	}
+
+	
+	// A test for the first slice
+	@Test
+	public void testAddingSingleNumber() throws StringCalculatorException {
+		//Arrange
+		StringCalculator calc = new StringCalculator();
+		String input = "76";
+		
+		//Act
+		int result = calc.add(input);
+		
+		//Assert
+		assertEquals(input, String.valueOf(result));
+	}
+	
+	// A test for the second slice
+	@Test
+	public void testAddingThreeNumbers() throws StringCalculatorException {
+		//Arrange
+		StringCalculator calc = new StringCalculator();
+		String input = "20,37,39";
+		int expectedResult = 20 + 37 + 39;
+		
+		//Act
+		int result = calc.add(input);
+		
+		//Assert
+		assertEquals(expectedResult, result);
+	}
+	
+	// A test for the third slice
+	@Test (expected = StringCalculatorException.class)
+	public void testAddingLetters() throws StringCalculatorException {
+		//Arrange
+		StringCalculator calc = new StringCalculator();
+		String input = "3,40,abc";
+		
+		//Act
+		int result = calc.add(input);
+	}
+	
+	// A test for the fourth slice
+	@Test (expected = StringCalculatorException.class)
+	public void testAddingNegativeNumbers() throws StringCalculatorException {
+		//Arrange
+		StringCalculator calc = new StringCalculator();
+		String input = "-100,2,65";
+		
+		//Act
+		int result = calc.add(input);
+	}
+	
+	// A test for the fifth slice
+	@Test
+	public void testAddingNewlinesAndCommas() throws StringCalculatorException {
+		//Arrange
+		StringCalculator calc = new StringCalculator();
+		String input = "123,40\n30";
+		int expectedResult = 123 + 40 + 30;
+		
+		//Act
+		int result = calc.add(input);
+		
+		//Assert
+		assertEquals(expectedResult, result);
+	}
+	
+	// A test for the sixth slice
+	@Test (expected = StringCalculatorException.class)
+	public void testConsecutiveCommaAndNewline() throws StringCalculatorException {
+		//Arrange
+		StringCalculator calc = new StringCalculator();
+		String input = "1,\n";
+		
+		//Act
+		int result = calc.add(input);
+		System.out.println(result);
 	}
 
 }
