7c154c593e8a2803e18acb7bb787a58a94efb7cc
mikkruot <mikkruot@MIKROTILAT1-237.student.yo.oulu.fi>
2017-09-27 16:41:33 +0300

    Assignment Finished

--- tests/StringCalculatorTest.java
+++ tests/StringCalculatorTest.java
@@ -5,8 +5,53 @@ import org.junit.Test;
 public class StringCalculatorTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testAddforViableInput1_2() throws StringCalculatorException {
+		
+		//Arrange
+		StringCalculator Calc = new StringCalculator();
+		
+		//Assert
+		assertEquals("1 + 2 = 3", 3, Calc.add("1,2"));
+		
 	}
+	
+	@Test
+	public void testAdd_shouldOnlyTake012() throws StringCalculatorException {
+		//Arrange
+		StringCalculator Calc = new StringCalculator();
+		
+		//Assert
+		assertEquals("1 + 2 + 3 = 0", 0, Calc.add("1,2,3"));
+		assertEquals("2 = 2", 2, Calc.add("2"));
+		assertEquals("empty input = 0", 0, Calc.add(""));
+		
+	}
+	
+	@Test
+	public void testAdd_shouldAcceptNewlines() throws StringCalculatorException {
+		//Arrange
+		StringCalculator Calc = new StringCalculator();
+		
+		//Assert
+		assertEquals("1 + 2 = 3", 3, Calc.add("1\n2"));
+		
+	}
+	@Test
+	public void testAdd_shouldNotAcceptConsecutiveLimiters() throws StringCalculatorException {
+		//Arrange
+		StringCalculator Calc = new StringCalculator();
+		assertEquals("1,\\n = 0", 0, Calc.add("1,\n"));
+		//Assert
+		
+	}
+	@Test (expected = Exception.class)
+	public void testAdd_shouldNotAcceptNegativeNumbers() throws StringCalculatorException {
+		//Arrange
+		StringCalculator Calc = new StringCalculator();
+				
+		//Assert
+		assertEquals("Should throw exception on -1", 0, Calc.add("-1"));
+	}
+	
 
 }
