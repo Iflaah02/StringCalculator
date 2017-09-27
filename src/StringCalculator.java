6dfed8295bce788346d88e0377cf79746a3c3de0
bhein <bhein@MIKROTILAT1-195.student.yo.oulu.fi>
2017-09-27 17:28:43 +0300

    Zwischenlösung

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,9 +1,75 @@
+import java.util.ArrayList;
 
 public class StringCalculator {
-	public int add(String numbersStr) {
+	private static String[] numbers;
+	public static int splitLength;
+	public static int lengthNumbers;
+	public int add(String numbersStr) throws StringCalculatorException {
 		// Returns the sum of the numbers given in numbersStr
+		
+		
+		numbers=getStringContent(numbersStr);
+		
+		//testLength(numbers);
+		
+		
+		
+		
+		
 		
 		// not yet implemented
 		return 0;
 	}
+	
+	private String[] getStringContent(String numbersStr) {			
+		
+		String[]split = numbersStr.split(",");
+		splitLength = split.length;
+		
+		return split;
+	}
+	
+	/*private void testLength(String[]split) throws StringCalculatorException {
+		if (split.length >2) {
+			throw new StringCalculatorException();
+		}
+	}*/
+	
+	public int sumOfNumbers() {
+		//Integer[] intNumber = new Integer[numbers.length];
+		int sum =0;
+		
+		for (int i=0; i<numbers.length;i++) {
+			//intNumber[i]=Integer.parseInt(numbers[i]);
+			sum = sum + Integer.parseInt(numbers[i]);
+		}
+		
+		return sum;
+	}
+	
+	public void newLines () {
+		ArrayList num =new ArrayList();
+		for (int i=0; i<numbers.length;i++) {
+			//intNumber[i]=Integer.parseInt(numbers[i]);
+			String split[]=numbers[i].split("\n");
+			if (split.length==2) {
+				num.add(split[0]);
+				num.add(split[1]);
+			} else {
+				num.add(split[0]);
+			}			
+			
+		}		
+		lengthNumbers = num.size();
+		
+	} 
+	
+	public void newLinesError(String numbersStr) throws StringCalculatorException {
+		if (numbersStr.contains(",\n")) {
+			throw new StringCalculatorException();
+			
+		}
+	}
+	
+	
 }
--- tests/StringCalculatorTest.java
+++ tests/StringCalculatorTest.java
@@ -4,9 +4,78 @@ import org.junit.Test;
 
 public class StringCalculatorTest {
 
+	
+	@Test
+	public void testSplit() throws StringCalculatorException {
+		StringCalculator calculator = new StringCalculator();
+		calculator.add("1,2");
+		int splitLength = calculator.splitLength;
+		assertEquals(2,splitLength);
+				
+	}
+	@Test
+	public void testSplit1() throws StringCalculatorException {
+		StringCalculator calculator = new StringCalculator();
+		calculator.add("1");
+		int splitLength = calculator.splitLength;
+		assertEquals(1,splitLength);
+				
+	}
+	
+	@Test
+	public void testSplit0() throws StringCalculatorException {
+		StringCalculator calculator = new StringCalculator();
+		calculator.add("");
+		int splitLength = calculator.splitLength;
+		assertEquals(1,splitLength);
+				
+	}
 	@Test
 	public void test() {
 		fail("Not yet implemented");
+	}
+	
+	/*@Test (expected = StringCalculatorException.class)
+	public void testLength() throws StringCalculatorException {
+		StringCalculator calculator = new StringCalculator();
+		calculator.add("1,2,3");
+				
+	}*/
+	
+	@Test
+	public void testSum() throws StringCalculatorException  {
+		StringCalculator calculator = new StringCalculator();
+		calculator.add("5,2,7");
+		
+		int sumOfNumbers = calculator.sumOfNumbers();
+		assertEquals(14,sumOfNumbers);
+	}
+	
+	@Test
+	public void testnewLine() throws StringCalculatorException  {
+		StringCalculator calculator = new StringCalculator();
+		calculator.add("5,2");
+		calculator.newLines();
+		
+		int withNewLine = calculator.lengthNumbers;
+		assertEquals(2,withNewLine);
+	}
+	@Test
+	public void testnewLine2() throws StringCalculatorException  {
+		StringCalculator calculator = new StringCalculator();
+		calculator.add("5\n3,2");
+		calculator.newLines();
+		
+		int withNewLine = calculator.lengthNumbers;
+		assertEquals(3,withNewLine);
+	}
+	@Test (expected = StringCalculatorException.class)
+	public void testnewLineError() throws StringCalculatorException  {
+		StringCalculator calculator = new StringCalculator();
+		
+		calculator.newLinesError("5,\n");
+		
+		
 	}
 
 }
