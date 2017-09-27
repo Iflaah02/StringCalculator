3b6281de23124e5dd27bed76af0ed40c5adb34ae
emolkose <emolkose@MIKROTILAT1-176.student.yo.oulu.fi>
2017-09-27 15:31:53 +0300

    Assignment Finished

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,9 +1,43 @@
+import java.util.*;
 
 public class StringCalculator {
-	public int add(String numbersStr) {
-		// Returns the sum of the numbers given in numbersStr
+	/** Returns the sum of the numbers given in numbersStr. */
+	public int add(String numbersStr) throws StringCalculatorException {
+		if (numbersStr.equals("")) {
+			return 0;
+		}
+		Integer[] intAr = get_intAr(numbersStr);
+		int sum = 0;
+		for (int i = 0; i < intAr.length; i++) {
+			sum += intAr[i];
+		}
+		return sum;
+	}
+	
+	public int str2int(String str) throws StringCalculatorException {
+		int num;
 		
-		// not yet implemented
-		return 0;
+		// To string
+		try {
+			num = Integer.parseInt(str);
+		} catch(NumberFormatException e) {
+			throw(new StringCalculatorException());
+		}
+		
+		// Check negative numbers
+		if (num < 0) {
+			throw(new StringCalculatorException());
+		}
+		
+		return num;
+	}
+	
+	public Integer[] get_intAr(String numbersStr) throws StringCalculatorException {
+		String[] strAr = numbersStr.split(",|\n");
+		ArrayList<Integer> intAr = new ArrayList<Integer>();
+		for (int i = 0; i < strAr.length; i++) {
+			intAr.add(str2int(strAr[i]));
+		}
+		return intAr.toArray(new Integer[intAr.size()]);
 	}
 }
--- tests/StringCalculatorTest.java
+++ tests/StringCalculatorTest.java
@@ -3,10 +3,80 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class StringCalculatorTest {
-
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test_add_sum_123() {
+		StringCalculator sc = new StringCalculator();
+		int sum = 0;
+		try {
+			sum = sc.add("1,2,3,3");
+		} catch(StringCalculatorException e) {
+			fail("Exception caught");
+		}
+		assertEquals("Sum wasn't 9", sum, 9);
 	}
-
+	
+	@Test
+	public void test_add_sum_empty() {
+		StringCalculator sc = new StringCalculator();
+		int sum = 0;
+		try {
+			sum = sc.add("");
+		} catch(StringCalculatorException e) {
+			fail("Exception caught");
+		}
+		assertEquals("Sum wasn't 0", sum, 0);
+	}
+	
+	@Test
+	public void test_str2int_positive() {
+		StringCalculator sc = new StringCalculator();
+		int num = 0;
+		try {
+			num = sc.str2int("10");
+		} catch(StringCalculatorException e) {
+			fail("Exception caught");
+		}
+		assertEquals("Sum wasn't 0", num, 10);
+	}
+	
+	@Test
+	public void test_str2int_negative() {
+		StringCalculator sc = new StringCalculator();
+		try {
+			sc.str2int("-10");
+			fail("No exception was caught");
+		} catch(StringCalculatorException e) {}
+	}
+	
+	@Test
+	public void test_str2int_invalid() {
+		StringCalculator sc = new StringCalculator();
+		try {
+			sc.str2int("kymmenen");
+			fail("No exception was caught");
+		} catch(StringCalculatorException e) {}
+	}
+	
+	@Test
+	public void test_get_intAr_valid() {
+		StringCalculator sc = new StringCalculator();
+		Integer[] intAr = {0, 0, 0};
+		try {
+			intAr = sc.get_intAr("0,1\n2");
+		} catch(StringCalculatorException e) {
+			fail("Exception caught");
+		}
+		assertEquals("intAr[0] wasn't 0", (int)intAr[0], 0);
+		assertEquals("intAr[1] wasn't 1", (int)intAr[1], 1);
+		assertEquals("intAr[2] wasn't 2", (int)intAr[2], 2);
+	}
+	
+	@Test
+	public void test_get_intAr_invalid() {
+		StringCalculator sc = new StringCalculator();
+		try {
+			sc.str2int("5,");
+			fail("No exception was caught");
+		} catch(StringCalculatorException e) {}
+	}
 }
