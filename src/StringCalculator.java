d0fc2e24f65e2ba69aef286bd5f1023fb33e89ee
maghaei <majid.aghaei1363@gmail.com>
2017-09-28 22:46:35 +0300

    Almost finished

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -3,42 +3,148 @@ import java.util.Scanner;
 
 public class StringCalculator {
 	
-	public int num1;
-	public int num2;
+	int num1 = 0;
+	int num2 = 0;
 	
-	int sum;
+	int sum = 0;
 	
-	public boolean getNumbers(String inputStr)
+	public void detectNumbers(String inputStr) throws StringCalculatorException
 	{
-		if (isEmpty(inputStr)) 
+		if (inputStr.isEmpty()) 
 			{
 				num1 = 0;
 				num2 = 0;
-				return true;
 			}
 		else 
 		{
-			String[] tokens = parseString(inputStr);
-			if (hasMoreThanTwoOperands(tokens)) 
+			if (hasValidInputs(inputStr))
 			{
-				System.out.println("The unknown amount of numbers have been detected.");
-				return false;
-			}
-			else 
+				if (hasOneNumber(inputStr))
 				{
-					int[] nums = getNumbersFromString(tokens);
-					num1 = nums[0];
-					num2 = nums[1];
-					return true;
+					num1 = Integer.parseInt(inputStr);
+					num2 = 0;
 				}
+				else
+				{
+					if (hasTwoNumbers(inputStr)) 
+					{
+						if (isInCorrectOrder(inputStr))
+						{
+							String[] tokens = parseString(inputStr);
+							int[] nums = convertStringToInteger(tokens);
+							num1 = nums[0];
+							num2 = nums[1];
+							if (num1 < 0 || num2 < 0) throw new StringCalculatorException("Negative numbers detected!");
+						}
+						else throw new StringCalculatorException("Wrong order detected!");
+					}
+					else throw new StringCalculatorException("Unknown amount of numbers or invalid usage detected!");
+				}
+			}
+			else throw new StringCalculatorException("Invalid input!");
 		}
-		
 	}
 	
-	public boolean isEmpty(String s)
+	public boolean hasOneNumber(String s)
 	{
-		if (s.isEmpty()) return true;
-		else return false;
+		boolean state = true;
+		char[] chars = s.toCharArray();
+		for(int i = 0; i < s.length(); i++)
+		{
+			switch(chars[i])
+			{
+				case '0': break;
+				case '1': break;
+				case '2': break;
+				case '3': break;
+				case '4': break;
+				case '5': break;
+				case '6': break;
+				case '7': break;
+				case '8': break;
+				case '9': break;
+				default: state = false; break;
+			}
+		}
+		return state;
+	}
+	
+	public boolean hasTwoNumbers(String s)
+	{
+		boolean state = false;
+		char[] chars = s.toCharArray();
+		int ctr = 0;
+		for(int i = 0; i < s.length(); i++)
+		{
+			if (chars[i] == ',') ctr++;
+		}
+		if (ctr == 1) state = true;
+		else state = false;
+		return state;
+	}
+	
+	public boolean hasValidInputs(String s)
+	{
+		boolean state = true;
+		char[] chars = s.toCharArray();
+		for(int i = 0; i < s.length(); i++)
+		{
+			switch(chars[i])
+			{
+				case '0':  break;
+				case '1':  break;
+				case '2':  break;
+				case '3':  break;
+				case '4':  break;
+				case '5':  break;
+				case '6':  break;
+				case '7':  break;
+				case '8':  break;
+				case '9':  break;
+				case ',':  break;
+				case '\n': break;
+				case '-':  break;
+				default: state = false; break;
+			}
+		}
+		return state;
+	}
+	
+	public boolean isDigit(char c)
+	{
+		boolean state = true;
+		switch(c)
+			{
+				case '0':  break;
+				case '1':  break;
+				case '2':  break;
+				case '3':  break;
+				case '4':  break;
+				case '5':  break;
+				case '6':  break;
+				case '7':  break;
+				case '8':  break;
+				case '9':  break;
+				default: state = false; break;
+			}
+		return state;
+	}
+	
+	public boolean isInCorrectOrder(String s)
+	{
+		boolean state = true;
+		char[] chars = s.toCharArray();
+		for(int i = 0; i < s.length(); i++)
+		{
+			if (chars[i] == ',')  
+				if (i>0 && i<s.length()-1)
+				{
+					if (isDigit(chars[i-1]) && isDigit(chars[i+1])) state = true;
+					else state = false;
+				}
+				else state = false;
+		}
+		return state;
 	}
 	
 	public String[] parseString(String s)
@@ -47,18 +153,12 @@ public class StringCalculator {
 		return tokens;
 	}
 	
-	public boolean hasMoreThanTwoOperands(String[] tokens)
-	{
-		if (tokens.length > 2) return true;
-		else return false;
-	}
-	
 	public boolean hasNewLine(String s)
 	{
 		return false;
 	}
 	
-	public int[] getNumbersFromString(String[] tokens)
+	public int[] convertStringToInteger(String[] tokens)
 	{
 		int[] nums = new int[2];
 		nums[0] = Integer.parseInt(tokens[0]);
@@ -67,18 +167,10 @@ public class StringCalculator {
 		return nums;
 	}
 	
-	public int add(String inputStr) 
+	public int add(String inputStr) throws StringCalculatorException
 	{
-		boolean state = getNumbers(inputStr);
-		if (state) 
-			{
-				sum = num1 + num2;
-				return sum;
-			}
-		else 
-			{
-				System.out.println("invalid inputs");
-				return 0;
-			}
+		detectNumbers(inputStr);
+		sum = num1 + num2;
+		return sum;
 	}
 }
--- tests/StringCalculatorTest.java
+++ tests/StringCalculatorTest.java
@@ -7,29 +7,119 @@ import org.junit.Test;
 public class StringCalculatorTest {
 	
 	@Test
-	public void test_isEmpty()
+	public void test_hasOneNumber()
 	{
 		//Arrange
 		StringCalculator cal = new StringCalculator();
 		
 		//Act
-		boolean result = cal.isEmpty("");
+		
 		
 		//Assert
-		assertTrue(result);
+		assertTrue(cal.hasOneNumber("355"));
+	}
+	
+	@Test
+	public void test_hasNotOneNumber()
+	{
+		//Arrange
+		StringCalculator cal = new StringCalculator();
+		
+		//Act
+		
+		
+		//Assert
+		assertFalse(cal.hasOneNumber("3,5"));
 	}
 
 	@Test
-	public void test_CheckIfIsNotEmpty()
+	public void test_hasTwoNumbers()
 	{
 		//Arrange
 		StringCalculator cal = new StringCalculator();
 		
 		//Act
-		boolean result = cal.isEmpty("1");
+		
 		
 		//Assert
-		assertFalse(result);
+		assertTrue(cal.hasTwoNumbers("10,20"));
+	}
+
+	@Test
+	public void test_hasMoreThanTwoNumbers()
+	{
+		//Arrange
+		StringCalculator cal = new StringCalculator();
+		
+		//Act
+		
+		
+		//Assert
+		assertFalse(cal.hasTwoNumbers("10,20,30"));
+	}
+
+	
+	@Test
+	public void test_hasValidInputs()
+	{
+		//Arrange
+		StringCalculator cal = new StringCalculator();
+				
+		//Act
+		
+				
+		//Assert
+		assertTrue(cal.hasValidInputs("55,18"));
+	}
+
+	@Test
+	public void test_isDigit()
+	{
+		//Arrange
+		StringCalculator cal = new StringCalculator();
+		
+		//Act
+		
+		//Assert
+		assertTrue(cal.isDigit('5'));
+	}
+
+	@Test
+	public void test_isNotDigit()
+	{
+		//Arrange
+		StringCalculator cal = new StringCalculator();
+		
+		//Act
+		
+		//Assert
+		assertFalse(cal.isDigit(';'));
+		assertFalse(cal.isDigit('@'));
+	}
+	
+	@Test
+	public void test_isInCorrectOrder()
+	{
+		//Arrange
+		StringCalculator cal = new StringCalculator();
+		
+		//Act
+		
+		//Assert
+		assertTrue(cal.isInCorrectOrder("360,420"));
+	}
+	
+	@Test
+	public void test_isNotInCorrectOrder()
+	{
+		//Arrange
+		StringCalculator cal = new StringCalculator();
+		
+		//Act
+		
+		//Assert
+		assertFalse(cal.isInCorrectOrder("134,"));
+		assertFalse(cal.isInCorrectOrder(",15"));
 	}
 	
 	@Test
@@ -47,44 +137,58 @@ public class StringCalculatorTest {
 	}
 	
 	@Test
-	public void test_GetNumberFromString()
+	public void test_convertStringToInteger()
 	{
 		//Arrange
 		StringCalculator cal = new StringCalculator();
 		
 		//Act
 		String[] tokens = cal.parseString("1,2");
-		int[] nums = cal.getNumbersFromString(tokens);
+		int[] nums = cal.convertStringToInteger(tokens);
 		
 		//Assert
 		assertEquals("num1 should be integer 1", 1, nums[0]);
 		assertEquals("num2 should be integer 2", 2, nums[1]);
 	}
 	
-	@Test
-	public void test_GetNumbers()
+	@Test //(expected = StringCalculatorException.class)
+	public void test_detectNumbers() throws StringCalculatorException
 	{
 		//Arrange
 		StringCalculator cal = new StringCalculator();
 		
 		//Act
-		//cal.getNumbers();
+		cal.detectNumbers("1,3");
 		
 		//Assert
-
+		assertEquals(1,cal.num1);
+		assertEquals(3,cal.num2);
 	}
 	
 	@Test
-	public void test_add()
+	public void test_negativeNumbers() throws StringCalculatorException
 	{
 		//Arrange
 		StringCalculator cal = new StringCalculator();
 		
 		//Act
-		int sum = cal.add("3,6");
+		int sum = cal.add("-5,6");
 		
 		//Assert
-		assertEquals(9, sum);
+		assertEquals(1, sum);
+	}
+	
+	@Test
+	public void test_add() throws StringCalculatorException
+	{
+		//Arrange
+		StringCalculator cal = new StringCalculator();
+		
+		//Act
+		int sum = cal.add("60,40");
+		
+		//Assert
+		assertEquals(100, sum);
 	}
 	
 }
