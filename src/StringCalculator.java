71b5dd8740d305ea45dccd55e70bacb46103958d
Miikka <miklpr@outlook.com>
2017-09-28 12:38:17 +0300

    Assignment Finished

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -2,8 +2,41 @@
 public class StringCalculator {
 	public int add(String numbersStr) {
 		// Returns the sum of the numbers given in numbersStr
+		if (numbersStr.length() == 0 || numbersStr == "") {
+			return 0;
+		}
 		
-		// not yet implemented
-		return 0;
+		inputValidityChecker(numbersStr);
+		int[] sumArr = sliceString(numbersStr);
+		
+		int sum = sumArr[0] + sumArr[1] + sumArr[2];
+		return sum;
 	}
-}
+	
+	public void inputValidityChecker(String theString) {
+		if (theString.endsWith(",") || theString.endsWith("\n")) {
+			//throw new StringCalculatorException();
+		}
+		
+		int[] arr2Check = sliceString(theString);
+		
+		for (int i = 0; i < arr2Check.length; i++) {
+			if (arr2Check[i] < 0) {
+				//throw new StringCalculatorException();
+			}
+		}
+	}
+	
+	public int[] sliceString(String theString) {
+		String[] numbers = theString.split(",|\\n");
+		if (numbers.length > 3) {
+			//throw new StringCalculatorException();
+		} else {
+			int[] numArr = new int[numbers.length];
+			for (int i = 0; i < numbers.length; i++) {
+				numArr[i] = Integer.parseInt(numbers[i]);
+			}
+			return numArr;
+			}
+		}
+	}
--- tests/StringCalculatorTest.java
+++ tests/StringCalculatorTest.java
@@ -4,9 +4,80 @@ import org.junit.Test;
 
 public class StringCalculatorTest {
 
-	@Test
+	/*@Test
 	public void test() {
 		fail("Not yet implemented");
+	}*/
+	
+	@Test
+	public void emptyArray() {
+		StringCalculator strCalc = new StringCalculator();
+		
+		assertEquals(0, strCalc.add(""));
+	}
+	
+	@Test
+	public void sumIsCorrectTest() {
+		StringCalculator strCalc = new StringCalculator();
+		
+		String str = "1,2,3";
+		
+		assertEquals(6, strCalc.add(str));
+	}
+	
+	@Test
+	public void sumIsCorrectTest2() {
+		StringCalculator strCalc = new StringCalculator();
+		
+		String str = "10,200,3000";
+		
+		assertEquals(3210, strCalc.add(str));
+	}
+	
+	@Test (expected = StringCalculatorException.class)
+	public void inputValidatorTestEndsWithIllegalChar() throws StringCalculatorException{
+		StringCalculator strCalc = new StringCalculator();
+		
+		String str = "1,2,";
+				
+		strCalc.add(str);
+	}
+	
+	@Test (expected = StringCalculatorException.class)
+	public void inputValidatorTestWithNegativeNumbers() throws StringCalculatorException{
+		StringCalculator strCalc = new StringCalculator();
+		
+		String str = "1,2,-3";
+				
+		strCalc.add(str);
+	}
+	
+	@Test (expected = StringCalculatorException.class)
+	public void sliceStringTestWithMoreThan3Numbers() throws StringCalculatorException{
+		StringCalculator strCalc = new StringCalculator();
+		
+		String str = "1,2,3,4";
+				
+		strCalc.sliceString(str);
+	}
+	
+	@Test (expected = StringCalculatorException.class)
+	public void inputValidatorTestWithNonNumbers() throws StringCalculatorException{
+		StringCalculator strCalc = new StringCalculator();
+		
+		String str = "one,two,three";
+				
+		strCalc.add(str);
+	}
+	
+	@Test
+	public void sliceStringTest() throws StringCalculatorException{
+		StringCalculator strCalc = new StringCalculator();
+		
+		String str = "1,2,3";
+		int[] compareStr = {1,2,3};
+		
+		assertEquals(compareStr, strCalc.sliceString(str));
 	}
 
 }
