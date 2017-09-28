df1a02b823272e7694325be7c359ee44fec9882f
Sabina Fataliyeva <s.fataliyeva@iteratia.com>
2017-09-28 14:44:36 +0400

    all methods added

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,22 +1,62 @@
+import java.util.ArrayList;
 
 public class StringCalculator {
-	// Returns the sum of the numbers given in numbersStr:
-	public int add(String numbersStr) {
-
-		return 0;
-	}
 
 	// First: adding 2 ready numbers
     public int sum(int num1, int num2) {
 	    return num1 + num2;
     }
 
-    // Second: adding an array of
-    public int sum(int[] numArray) {
+    // Second: adding any quantity of numbers
+    public int sum(ArrayList<Integer> numArray) {
         int result = 0;
         for(int number : numArray) {
             result += number;
         }
         return result;
+    }
+
+    // Third: extracting numbers from the string
+    // This is a high-value feature!
+    public ArrayList<Integer> getNumbers(String numbersStr) throws StringCalculatorException{
+        // dividing the string by "\n" and ","
+        String[] numbersArray = numbersStr.split("[\n,]");
+
+        // process the array and create the final list of numbers
+        ArrayList<Integer> numbersList = new ArrayList<Integer>();
+
+        for(String numberString : numbersArray) {
+            if(numberString.equals("")) { //happens when there is no number between "," and "\n". Or when it's an empty string
+                if(numbersArray.length == 1) { // empty string case
+                    numbersList.add(0);
+                }
+                else {  // cases like 1,\n5 or 1\n,5
+                    throw new StringCalculatorException();
+                }
+            }
+            else {
+                int number = -1;
+                try {
+                    number = Integer.parseInt(numberString);
+                }
+                catch (Exception e) {
+                    throw new StringCalculatorException(); // the string was not a number
+                }
+
+                if(number < 0) {
+                    throw new StringCalculatorException(); // throwing exceptions for negative numbers
+                }
+
+                // if all the checks have been successful, let's add the number to our final List:
+                numbersList.add(number);
+            }
+        }
+        return numbersList;
+    }
+
+    // Final slice!
+    // Returns the sum of the numbers given in numbersStr:
+    public int add(String numbersStr) throws StringCalculatorException {
+        return sum(getNumbers(numbersStr));
     }
 }
--- tests/StringCalculatorTest.java
+++ tests/StringCalculatorTest.java
@@ -3,6 +3,8 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 public class StringCalculatorTest {
 	private StringCalculator calculator;
 
@@ -21,9 +23,55 @@ public class StringCalculatorTest {
 	@Test
 	public void testSumAnyQuantityOfNumbers() {
 		// test1: let's test the sum of 4 numbers. (the next low-hanging fruit)
-		int numbers[] = {1, 2, 3, 4};
+		ArrayList<Integer> numbers = new ArrayList<Integer>();
+		numbers.add(1);
+		numbers.add(2);
+		numbers.add(3);
+		numbers.add(4);
+
 		int result = calculator.sum(numbers);
 		assertEquals(10, result);
 	}
 
+	// I really like to test bad cases first! So:
+	// test 3: get numbers list for empty string
+	@Test
+	public void testGetNumbersWithEmptyString() throws StringCalculatorException {
+		ArrayList<Integer> numberList = calculator.getNumbers("");
+		assertNotNull(numberList);
+		assertEquals(1, numberList.size());
+		int number = numberList.get(0);
+		assertEquals(0, number);
+	}
+
+	// test 4: bad structure of string. case 1: "1\n,5"
+	@Test(expected = StringCalculatorException.class)
+	public void testGetNumbersWithInvalidStringPatternCase1() throws StringCalculatorException {
+		ArrayList<Integer> numberList = calculator.getNumbers("1\n,5");
+	}
+
+	// test 5: negative number in the string
+	@Test(expected = StringCalculatorException.class)
+	public void testGetNumbersWithNegativeNumberInString() throws StringCalculatorException {
+		ArrayList<Integer> numberList = calculator.getNumbers("1\n-150");
+	}
+
+	// test 6: letters or special characters instead or number
+	@Test(expected = StringCalculatorException.class)
+	public void testGetNumbersInvalidNumber() throws StringCalculatorException {
+		ArrayList<Integer> numberList = calculator.getNumbers("3mh%#&");
+	}
+
+	// test 7: success!
+	@Test
+	public void testGetNumbersSuccess() throws StringCalculatorException {
+		ArrayList<Integer> numberList = calculator.getNumbers("1,2\n3,4,5\n6");
+	}
+
+	// test 8: testing the final method!
+	@Test
+	public void testAdd() throws StringCalculatorException{
+		int result = calculator.add("1,2\n3,4,5\n6");
+		assertEquals(21, result);
+	}
 }
