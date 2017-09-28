63b8d06c0831b24b2c336a0fa8e6d39994e14348
Annukka Törmälä <tormala.annukka@gmail.com>
2017-09-28 14:59:19 +0300

    Assignment	Finished

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,9 +1,32 @@
 
 public class StringCalculator {
 	public int add(String numbersStr) {
-		// Returns the sum of the numbers given in numbersStr
-		
-		// not yet implemented
-		return 0;
+
+		String arr[] = numbersStr.trim().split(",\\s+"); // ",\\s+" "[a-zA-Z ]+"
+		int sum = 0;
+
+		for (int i = 0; i < arr.length; i++) {
+
+			if (arr.length > 2) {
+				return 0;
+
+			} else if (numbersStr == " ") {
+				return 0;
+
+			} else {
+
+				int parsedNumber = Integer.parseInt(arr[i]);
+
+				if (parsedNumber < 0) {
+					return 0;
+				}
+
+				sum = sum + parsedNumber;
+				System.out.printf("sum: " + sum + "\n");
+			}
+
+		}
+
+		return sum;
 	}
 }
--- tests/StringCalculatorTest.java
+++ tests/StringCalculatorTest.java
@@ -5,8 +5,55 @@ import org.junit.Test;
 public class StringCalculatorTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void sumOneNumberTest() {
+		StringCalculator calculator = new StringCalculator();
+		int expectedOutput = 2;
+
+		int value = calculator.add("2");
+		assertEquals(value, expectedOutput);
 	}
+
+	@Test
+	public void passTestIfEmptyStringTest() {
+		StringCalculator calculator = new StringCalculator();
+
+		int expectedOutput = 0;
+		int value = calculator.add(" ");
+
+		assertEquals(value, expectedOutput);
+	}
+
+	@Test
+	public void SumTwoNumbersTest() {
+		StringCalculator calculator = new StringCalculator();
+
+		int expectedOutput = 10;
+		int value = calculator.add("4, 6");
+
+		assertEquals(value, expectedOutput);
+	}
+
+	@Test
+	public void DontAcceptMoreThanTwoNumbersTest() {
+		StringCalculator calculator = new StringCalculator();
+
+		int expectedOutput = 15;
+		int value = calculator.add("2, 10, 3");
+		
+		assertNotEquals(value, expectedOutput);
+
+	}
+	
+	@Test
+	public void DontAcceptNegativeNumbersTest(){
+		StringCalculator calculator = new StringCalculator();
+		
+		int expectedOutput = -5;
+		int value = calculator.add("-2, -3");
+		
+		assertNotEquals(value, expectedOutput);
+		
+	}
+	
 
 }
