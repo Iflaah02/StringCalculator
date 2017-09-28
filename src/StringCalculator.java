3a803a653949e466ce28a2b76bae00d3cf8f548a
chouaibHamek <chouaib.hamek@student.oulu.fi>
2017-09-28 15:08:08 +0300

    Add regular exp to verify Input pattern and adequate tests

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -10,23 +10,27 @@ public class StringCalculator {
 		result = res;
 	}
 	
-	public int add(String numbersStr) {
+	public int add(String numbersStr) throws StringCalculatorException  {
 		// Returns the sum of the numbers given in numbersStr
 		int res = 0;
-		if (numbersStr != "") {	
-		String [] numbers = numbersStr.split(",");
-			for (int i=0; i<numbers.length; i++) {
-				if (numbers[i].contains("\n")) {
-					String [] numbersEndl = numbers[i].split("\n");
-					for (int j=0; j<numbersEndl.length; j++) {
-						res+=Integer.parseInt(numbersEndl[j]);
+		if (numbersStr.matches("([0-9]+((,|\\n)[0-9]+)*)*")) { // Input is matching
+			if (numbersStr != "") {	
+				String [] numbers = numbersStr.split(",");
+					for (int i=0; i<numbers.length; i++) {
+						if (numbers[i].contains("\n")) {
+							String [] numbersEndl = numbers[i].split("\n");
+							for (int j=0; j<numbersEndl.length; j++) {
+								res+=Integer.parseInt(numbersEndl[j]);
+							}
+						} else {
+							res+= Integer.parseInt(numbers[i]);					
+						}
 					}
-				} else {
-					res+= Integer.parseInt(numbers[i]);					
 				}
-			}
+				setResult(res);
+				return result;
+		} else {
+			throw new StringCalculatorException();
 		}
-		setResult(res);
-		return result;
 	}
 }
--- tests/StringCalculatorTest.java
+++ tests/StringCalculatorTest.java
@@ -5,50 +5,62 @@ import org.junit.Test;
 public class StringCalculatorTest {
 
 	@Test
-	public void addEmpty() {
+	public void addEmpty() throws StringCalculatorException {
 		StringCalculator calc = new StringCalculator();
 		assertEquals("Error with empty string", 0, calc.add(""));
 	}
 	
 	@Test
-	public void addOne() {
+	public void addOne() throws StringCalculatorException {
 		StringCalculator calc = new StringCalculator();
 		assertEquals("Error with empty string", 7, calc.add("7"));
 	}
 	
 	@Test
-	public void addTwo() {
+	public void addTwo() throws StringCalculatorException {
 		StringCalculator calc = new StringCalculator();
 		assertEquals("Error with empty string", 6, calc.add("1,5"));
 	}
 	
 	@Test
-	public void addThree() {
+	public void addThree() throws StringCalculatorException {
 		StringCalculator calc = new StringCalculator();
 		assertEquals("Error with empty string", 10, calc.add("2,3,5"));
 	}
 	
 	@Test
-	public void addMultiDigitNumbers() {
+	public void addMultiDigitNumbers() throws StringCalculatorException {
 		StringCalculator calc = new StringCalculator();
 		assertEquals("Error with empty string", 1111, calc.add("111,999,1"));
 	}
 	
 	@Test
-	public void addMoreThanThreeNumbers() {
+	public void addMoreThanThreeNumbers() throws StringCalculatorException {
 		StringCalculator calc = new StringCalculator();
 		assertEquals("Error with empty string", 80, calc.add("7,3,50,12,8"));
 	}
 	
 	@Test
-	public void addWithEndl() {
+	public void addWithEndl() throws StringCalculatorException {
 		StringCalculator calc = new StringCalculator();
 		assertEquals("Error with empty string", 80, calc.add("7,3\n50,12\n8"));
 	}
 	
 	@Test
-	public void addWithEndlOnly() {
+	public void addWithEndlOnly() throws StringCalculatorException {
 		StringCalculator calc = new StringCalculator();
 		assertEquals("Error with empty string", 174, calc.add("7\n3\n10\n154"));
+	}
+	
+	@Test (expected = StringCalculatorException.class)
+	public void addWithWrongInput() throws StringCalculatorException {
+		StringCalculator calc = new StringCalculator();
+		calc.add("7,4\n");
+	}
+	
+	@Test (expected = StringCalculatorException.class)
+	public void addWithWrongInput1() throws StringCalculatorException {
+		StringCalculator calc = new StringCalculator();
+		calc.add("7,4,");
 	}
 }
