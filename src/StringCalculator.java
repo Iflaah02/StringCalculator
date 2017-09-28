4b6639e0d8a94acb1909d280e945345d557f8ad6
steffe93 <grundstrom.stefan93@gmail.com>
2017-09-28 15:00:07 +0300

    Assignment #3

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,8 +1,14 @@
 
+import java.util.Scanner;
 public class StringCalculator {
+
 	public int add(String numbersStr) {
 		// Returns the sum of the numbers given in numbersStr
+		Scanner reader = new Scanner(System.in);
+		System.out.println("Enter a number between 0-2: ");
+		int num = reader.nextInt();
 		
+		System.out.println(num);
 		// not yet implemented
 		return 0;
 	}
