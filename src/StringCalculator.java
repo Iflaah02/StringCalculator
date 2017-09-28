a8187f88f5bcf0fc9f38d685a0b51043d187daff
jatikka <Adsam1990!>
2017-09-28 20:32:11 +0300

    Assignment 3 finished

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,9 +1,16 @@
 
 public class StringCalculator {
+	
 	public int add(String numbersStr) {
-		// Returns the sum of the numbers given in numbersStr
-		
-		// not yet implemented
-		return 0;
+		if (numbersStr.length() == 0) 
+			return 0;
+	
+	String[] values = numbersStr.split(",|\n");
+
+	int total = 0;
+	for (String value : values) {
+		total += Integer.valueOf(value);
+		}
+		return total;
 	}
 }
--- tests/StringCalculatorTest.java
+++ tests/StringCalculatorTest.java
@@ -3,10 +3,36 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class StringCalculatorTest {
-
+    private StringCalculator calculator = new StringCalculator();
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void whenSingleStringCharacterThenReturnValue() {
+		addStringValue("", 0);
+		addStringValue("0", 0);
+		addStringValue("1", 1);
+		addStringValue("2", 2);
+	}
+	@Test
+	public void whenMultipleStringCharactersWithCommaThenReturnValue() {
+		addStringValue("0,1", 1);
+		addStringValue("1,1", 2);
+		addStringValue("1,2", 3);
+		addStringValue("2,2", 4);
+	}
+	@Test
+	public void whenMultipleStringCharactersWithNewLineThenReturnValue() {
+		addStringValue("1\n1", 2);
+		addStringValue("2\n2", 4);
+		addStringValue("3\n3", 6);
+	}
+	@Test
+	public void whenMultipleStringCharactersWithNewLineAndCommaThenReturnValue() {
+		addStringValue("1\n1,3", 5);
+		addStringValue("2,2\n3", 7);
+		addStringValue("3\n3", 6);
+	}
+
+	private void addStringValue(String numbersStr, int i) {
+		assertEquals(i, calculator.add(numbersStr));
 	}
 
 }
