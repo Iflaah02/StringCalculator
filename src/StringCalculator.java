6168e5eb045d636084e71c23a7ece6e65bdde3e3
maghaei <maghaei@ts137-pc10.student.yo.oulu.fi>
2017-09-28 14:37:18 +0300

    Partly Finished

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,9 +1,84 @@
+import java.io.IOException;
+import java.util.Scanner;
 
 public class StringCalculator {
-	public int add(String numbersStr) {
-		// Returns the sum of the numbers given in numbersStr
+	
+	public int num1;
+	public int num2;
+	
+	int sum;
+	
+	public boolean getNumbers(String inputStr)
+	{
+		if (isEmpty(inputStr)) 
+			{
+				num1 = 0;
+				num2 = 0;
+				return true;
+			}
+		else 
+		{
+			String[] tokens = parseString(inputStr);
+			if (hasMoreThanTwoOperands(tokens)) 
+			{
+				System.out.println("The unknown amount of numbers have been detected.");
+				return false;
+			}
+			else 
+				{
+					int[] nums = getNumbersFromString(tokens);
+					num1 = nums[0];
+					num2 = nums[1];
+					return true;
+				}
+		}
 		
-		// not yet implemented
-		return 0;
+	}
+	
+	public boolean isEmpty(String s)
+	{
+		if (s.isEmpty()) return true;
+		else return false;
+	}
+	
+	public String[] parseString(String s)
+	{
+		String tokens[] = s.split(",");
+		return tokens;
+	}
+	
+	public boolean hasMoreThanTwoOperands(String[] tokens)
+	{
+		if (tokens.length > 2) return true;
+		else return false;
+	}
+	
+	public boolean hasNewLine(String s)
+	{
+		return false;
+	}
+	
+	public int[] getNumbersFromString(String[] tokens)
+	{
+		int[] nums = new int[2];
+		nums[0] = Integer.parseInt(tokens[0]);
+		nums[1] = Integer.parseInt(tokens[1]);
+		
+		return nums;
+	}
+	
+	public int add(String inputStr) 
+	{
+		boolean state = getNumbers(inputStr);
+		if (state) 
+			{
+				sum = num1 + num2;
+				return sum;
+			}
+		else 
+			{
+				System.out.println("invalid inputs");
+				return 0;
+			}
 	}
 }
--- tests/StringCalculatorTest.java
+++ tests/StringCalculatorTest.java
@@ -1,12 +1,90 @@
 import static org.junit.Assert.*;
 
+import java.util.Scanner;
+
 import org.junit.Test;
 
 public class StringCalculatorTest {
-
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test_isEmpty()
+	{
+		//Arrange
+		StringCalculator cal = new StringCalculator();
+		
+		//Act
+		boolean result = cal.isEmpty("");
+		
+		//Assert
+		assertTrue(result);
 	}
 
+	@Test
+	public void test_CheckIfIsNotEmpty()
+	{
+		//Arrange
+		StringCalculator cal = new StringCalculator();
+		
+		//Act
+		boolean result = cal.isEmpty("1");
+		
+		//Assert
+		assertFalse(result);
+	}
+	
+	@Test
+	public void test_ParseString()
+	{
+		//Arrange
+		StringCalculator cal = new StringCalculator();
+		
+		//Act
+		String[] tokens = cal.parseString("1,2");
+		
+		//Assert
+		assertEquals("should create 1 by splitting the input string by ,", "1", tokens[0]);
+		assertEquals("should create 2 by splitting the input string by ,", "2", tokens[1]);
+	}
+	
+	@Test
+	public void test_GetNumberFromString()
+	{
+		//Arrange
+		StringCalculator cal = new StringCalculator();
+		
+		//Act
+		String[] tokens = cal.parseString("1,2");
+		int[] nums = cal.getNumbersFromString(tokens);
+		
+		//Assert
+		assertEquals("num1 should be integer 1", 1, nums[0]);
+		assertEquals("num2 should be integer 2", 2, nums[1]);
+	}
+	
+	@Test
+	public void test_GetNumbers()
+	{
+		//Arrange
+		StringCalculator cal = new StringCalculator();
+		
+		//Act
+		//cal.getNumbers();
+		
+		//Assert
+
+	}
+	
+	@Test
+	public void test_add()
+	{
+		//Arrange
+		StringCalculator cal = new StringCalculator();
+		
+		//Act
+		int sum = cal.add("3,6");
+		
+		//Assert
+		assertEquals(9, sum);
+	}
+	
 }
