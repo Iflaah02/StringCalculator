86bb0fede924811c7f4604b7199b1e870886996c
dtoniuc <dtoniuc@ts137-pc2.student.yo.oulu.fi>
2017-09-28 13:25:06 +0300

    ADD: String parser

---
+++ src/StringParser.java
@@ -0,0 +1,21 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class StringParser {
+	public static List<Integer> parseIntegerList(String numberStr, String separator) throws NumberFormatException{
+		List<Integer> argumentList = new LinkedList<Integer>();
+		
+		if(numberStr.isEmpty()) {
+			return argumentList;
+		}
+		else {
+			String arguments[] = numberStr.split(separator);
+			for(int i = 0; i < arguments.length; i++) {
+				int argument = Integer.parseInt(arguments[0]);
+				argumentList.add(argument);
+			}
+		}
+		
+		return argumentList;
+	}
+}
---
+++ tests/StringParserTest.java
@@ -0,0 +1,83 @@
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Test;
+
+public class StringParserTest {
+	
+	@Test
+	public void parseIntegerList_emptyString_emptyArray() {
+		//Init
+		String input = "";
+		
+		//Act
+		List<Integer> result = StringParser.parseIntegerList(input, ",");
+		
+		//Test
+		assertTrue(result.isEmpty());
+	}
+	
+	@Test
+	public void parseIntegerList_oneIntegerString_oneElementArray() {
+		//Init
+		String input = "12";
+		
+		//Act
+		List<Integer> result = StringParser.parseIntegerList(input, ",");
+		
+		//Test
+		assertEquals(1, result.size());
+	}
+	
+	@Test
+	public void parseIntegerList_multipleIntegerString_multipleElementArray() {
+		//Init
+		String input = "12,13,15,20";
+		
+		//Act
+		List<Integer> result = StringParser.parseIntegerList(input, ",");
+		
+		//Test
+		assertEquals(4, result.size());
+	}
+	
+	@Test
+	public void parseIntegerList_oneIntegerString_correctValue() {
+		//Init
+		String input = "12";
+		
+		//Act
+		List<Integer> result = StringParser.parseIntegerList(input, ",");
+		
+		//Test
+		Integer expected = 12;
+		Integer actual = result.get(0);
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void parseIntegerList_multipleIntegerString_correctValues() {
+		//Init
+		String input = "12,13";
+		
+		//Act
+		List<Integer> result = StringParser.parseIntegerList(input, ",");
+		
+		//Test
+		Integer value1 = 12;
+		Integer value2 = 12;
+		Integer actual1 = result.get(0);
+		Integer actual2 = result.get(0);
+		assertTrue(value1 == actual1 && value2 == actual2);
+	}
+	
+	@Test(expected = NumberFormatException.class) 
+	public void parseIntegerList_invalidString_throwException() {
+		//Init
+		String input = "dsadas";
+		
+		//Act
+		List<Integer> result = StringParser.parseIntegerList(input, ",");
+	}
+}
