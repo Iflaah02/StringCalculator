5824fd0cb6e7a899df063f01c98efa0b45e93316
jnyyssol <jnyyssol@ts137-pc26.student.yo.oulu.fi>
2017-09-28 16:14:01 +0300

    Assignment Finished

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,9 +1,89 @@
+import java.util.Scanner;
 
 public class StringCalculator {
+	
 	public int add(String numbersStr) {
 		// Returns the sum of the numbers given in numbersStr
 		
-		// not yet implemented
-		return 0;
+		int sum = 0;
+		int ncount = 0;
+		
+		try {
+			ncount = numberCount(numbersStr);
+		} catch (StringCalculatorException exception){
+			System.out.println("Invalid input.");
+			return 0;
+		}
+		
+		int[] ints = getIntegers(numbersStr, ncount);
+
+		for(int i : ints) {
+			sum = sum + i;
+		}
+		
+		return sum;
 	}
+
+	/*public String readInput() 
+	{
+		String str = "";
+		
+		System.out.println("Give up to three numbers: ");
+		Scanner sc = new Scanner(System.in);
+		str = sc.nextLine();
+	
+		return str;
+	}*/
+	
+	public int numberCount(String str) throws StringCalculatorException
+	{
+		int com = 0;
+		
+		for(int i = 0; i < str.length(); i++) {
+			if(!Character.isDigit(str.charAt(i))) {
+				if(!Character.isWhitespace(str.charAt(i))) {
+					if(str.charAt(i) != ',') {
+						throw new StringCalculatorException();
+					}
+					else com++;
+				}
+				else if (str.charAt(i) == '\n') com++;
+			}
+		}
+		if (com+1 > 3) {
+			throw new StringCalculatorException();
+		}
+			
+		
+		return com+1;
+	}
+	
+	public int[] getIntegers(String str, int count) 
+	{
+		int[] numbers = new int[count];
+		String sub = "";
+		int calc=0;
+		
+		for(int i = 0; i < str.length(); i++) {
+			if(Character.isDigit(str.charAt(i))) {
+				sub = sub + str.charAt(i);
+			}
+			else
+			{
+				if (!sub.isEmpty()) {
+					numbers[calc] = Integer.parseInt(sub);
+					sub = "";
+					calc++;
+				}
+			}
+			if (i == (str.length()-1)) {
+				numbers[calc] = Integer.parseInt(sub);
+			}
+		}
+		
+		return numbers;
+	}
+	
 }
+
+	
--- tests/StringCalculatorTest.java
+++ tests/StringCalculatorTest.java
@@ -4,9 +4,57 @@ import org.junit.Test;
 
 public class StringCalculatorTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
 
+	/*@Test
+	public void testStringCalculator_InputCorrect() {
+		
+		StringCalculator calc = new StringCalculator();
+		
+		String testr = calc.readInput();
+		
+		assertEquals("2, 2", testr);
+	}*/
+	
+	@Test (expected = StringCalculatorException.class)
+	public void testStringCalculator_NumberTest() throws StringCalculatorException {
+		StringCalculator calc = new StringCalculator();
+		
+		String testr = "2ab";
+		
+		calc.numberCount(testr);
+	}
+	
+	@Test 
+	public void testStringCalculator_NumberCount() throws StringCalculatorException {
+		StringCalculator calc = new StringCalculator();
+		
+		String testr = "22,2\n 22";
+		
+		int i = calc.numberCount(testr);
+		
+		assertEquals(i, 3);
+	}
+	
+	@Test
+	public void testStringCalculator_ArrayCorrect() {
+		
+		StringCalculator calc = new StringCalculator();
+		
+		int[] arr1 = calc.getIntegers("26, 19 \n 2", 3);
+		
+		int[] arr2 = {26, 19, 2};
+		
+
+		assertArrayEquals(arr1, arr2);
+	}
+	
+	@Test
+	public void testStringCalculator_AddCorrect() {
+		
+		StringCalculator calc = new StringCalculator();
+		
+		int testint = calc.add("3, 2 \n22");
+		
+		assertEquals(27, testint);
+	}
 }
