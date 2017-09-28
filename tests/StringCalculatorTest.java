4eea648a4e037e9f21ab437c9520f7acfad84186
jukoski <jukoski@ts137-pc23.student.yo.oulu.fi>
2017-09-28 14:58:11 +0300

    Code works with single digits. Didn't have time to code for multiple digits
    Juha-Pekka Koski
    id: 1505465

--- tests/StringCalculatorTest.java
+++ tests/StringCalculatorTest.java
@@ -5,8 +5,74 @@ import org.junit.Test;
 public class StringCalculatorTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test_emptystring() throws StringCalculatorException{
+		
+		//empty strings should return 0
+		 StringCalculator outcome = new StringCalculator();
+
+		 int  result= outcome.countnumbers("");
+
+		 assertEquals("Should be an empty string",0, result); 
+		
+		//test 1 number
+		//test 2 numbers¨
+		//test3 numbers
+		//test more than 3 numbers
+		//test empty string
+		//test negative numbers or letters
+		//test newlines between numbers
+		//test lines and commas back to back
+		
+	}
+	@Test
+	public void test_with_one_number() throws StringCalculatorException{
+		
+		//empty strings should return 0
+		 StringCalculator outcome = new StringCalculator();
+
+		 int  result= outcome.countnumbers("5");
+
+		 assertEquals("Doesn't add up",5, result); 
+	}
+	@Test
+	public void test_with_multiple_numbers() throws StringCalculatorException{
+		
+		
+		 StringCalculator outcome = new StringCalculator();
+
+		 int  result= outcome.countnumbers("1,2,3,4,5,6,7,8,9");
+
+		 assertEquals("Doesn't add up",45, result); 
+	}
+	@Test (expected = StringCalculatorException.class)
+	public void test_negativevalues() throws StringCalculatorException {
+		
+		
+		 StringCalculator outcome = new StringCalculator();
+
+		 outcome.countnumbers("1,3,-1");
+
+		
+	}
+	@Test (expected = StringCalculatorException.class)
+	public void test_invalid_inputs() throws StringCalculatorException {
+		
+		
+		 StringCalculator outcome = new StringCalculator();
+
+		 outcome.countnumbers("1,0,6,b");
+
+		
+	}
+	@Test (expected = StringCalculatorException.class)
+	public void test_comma_and_newline_backtoback_inputs() throws StringCalculatorException {
+		
+		
+		 StringCalculator outcome = new StringCalculator();
+
+		 outcome.countnumbers("\n,45");
+
+		
 	}
 
 }
