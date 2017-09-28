6285d23de0da2e618e648d5385dc4ebf5ecf6955
chouaibHamek <chouaib.hamek@student.oulu.fi>
2017-09-28 14:17:28 +0300

    Implement the Add method to accept three or more number and write adequate tests

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,9 +1,25 @@
 
 public class StringCalculator {
+	private int result;
+	
+	public StringCalculator () {
+		result = 0;
+	}
+	
+	private void setResult(int res) {
+		result = res;
+	}
+	
 	public int add(String numbersStr) {
 		// Returns the sum of the numbers given in numbersStr
-		
-		// not yet implemented
-		return 0;
+		int res = 0;
+		if (numbersStr != "") {	
+		String [] numbers = numbersStr.split(",");
+			for (int i=0; i<numbers.length; i++) {
+				res+= Integer.parseInt(numbers[i]);
+			}
+		}
+		setResult(res);
+		return result;
 	}
 }
--- tests/StringCalculatorTest.java
+++ tests/StringCalculatorTest.java
@@ -5,8 +5,38 @@ import org.junit.Test;
 public class StringCalculatorTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void addEmpty() {
+		StringCalculator calc = new StringCalculator();
+		assertEquals("Error with empty string", 0, calc.add(""));
 	}
-
+	
+	@Test
+	public void addOne() {
+		StringCalculator calc = new StringCalculator();
+		assertEquals("Error with empty string", 7, calc.add("7"));
+	}
+	
+	@Test
+	public void addTwo() {
+		StringCalculator calc = new StringCalculator();
+		assertEquals("Error with empty string", 6, calc.add("1,5"));
+	}
+	
+	@Test
+	public void addThree() {
+		StringCalculator calc = new StringCalculator();
+		assertEquals("Error with empty string", 10, calc.add("2,3,5"));
+	}
+	
+	@Test
+	public void addMultiDigitNumbers() {
+		StringCalculator calc = new StringCalculator();
+		assertEquals("Error with empty string", 1111, calc.add("111,999,1"));
+	}
+	
+	@Test
+	public void addMoreThanThreeNumbers() {
+		StringCalculator calc = new StringCalculator();
+		assertEquals("Error with empty string", 80, calc.add("7,3,50,12,8"));
+	}
 }
