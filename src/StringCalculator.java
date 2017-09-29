dd46e6d5f6d66e4adeda4640615469490e599524
valeriikan <valerii.kan1@gmail.com>
2017-09-29 04:25:56 +0300

    Assignment	Finished

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,9 +1,62 @@
 
 public class StringCalculator {
-	public int add(String numbersStr) {
-		// Returns the sum of the numbers given in numbersStr
+	
+	public int add(String numbersStr) throws StringCalculatorException {
 		
-		// not yet implemented
-		return 0;
+		String[] numbers = splitInput(numbersStr);
+		int sum = 0;
+		
+		for (int i=0; i<numbers.length; i++) {
+			if (validateNumber(numbers[i])) {
+				int value = transferToInt(numbers[i]);
+				if (validateNonNegative(value)) {
+					sum += value;
+				}
+			}
+		}
+		
+		return sum;
 	}
+
+	// splits input string to separate string symbols removing breaks and commas
+	public String[] splitInput(String input) throws StringCalculatorException {
+	
+		String nobreaks = input.replace('\n', ',');
+		String[] splitted = nobreaks.split(",");
+	
+		for (int i=0; i<splitted.length; i++) {
+			if (splitted[i].length() == 0) {
+				throw new StringCalculatorException();
+			}
+		}
+
+		return splitted;
+	}
+
+	// check if the element can be parse to integer
+	public boolean validateNumber(String element)  throws StringCalculatorException  {
+		try {
+			Integer.parseInt(element);
+		} catch (NumberFormatException exception) {
+			throw new StringCalculatorException();
+		} catch (NullPointerException exception) {
+			throw new StringCalculatorException();
+		}
+		return true;
+	}
+	
+	// check if the element is not negative
+	public boolean validateNonNegative(int element) throws StringCalculatorException {
+		if (element >= 0) {
+			return true;
+		} else {
+			throw new StringCalculatorException();
+		}
+	}
+	
+	// transforms number in string format to int
+	public int transferToInt(String string) {
+		return Integer.parseInt(string);
+	}
+	
 }
