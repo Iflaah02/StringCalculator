3e7a38b9733c3477a0cc1a5ea23c4885100c6b71
Sgathec <not-a-real-address@fakemail.com>
2017-10-04 17:33:03 +0300

    Refactoring finished (no parameter objects, please see the comments)

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,4 +1,5 @@
-
+// There didn't appear to be an opportunity to introduce a parameter object due
+// to a lack of multiparameter methods
 public class StringCalculator {
 	
 	private String string; 
@@ -7,39 +8,16 @@ public class StringCalculator {
 		
 		string = numbersStr;
 		
-		String[] numbers = splitByCommasOrNewlines(string);
+		String[] numbers = StringCalculationParser.splitByCommasOrNewlines(string);
 		
 		int result = 0;
 		
 		for(String number : numbers) {
 			
-			result += validateNumber(number);
+			result += StringCalculationParser.validateNumber(number);
 			
 		}
 	
-		return result;
-	}
-	
-	private String[] splitByCommasOrNewlines(String numbers) throws StringCalculatorException {
-		String[] result = numbers.split("[,\\n]");
-		if (numbers.matches(".[\n,][\n,]")) { // Sixth slice implementation (just the if branch)
-			throw new StringCalculatorException("Malformed input");
-		}
-		return result;
-	}
-	
-	private int validateNumber(String number) throws StringCalculatorException {
-		int result;
-		try {
-			result = Integer.valueOf(number);
-			if(result < 0 || number.isEmpty()) {
-				throw new StringCalculatorException("Negative numbers aren't allowed");
-			}
-		}
-		catch(NumberFormatException e) {
-			throw new StringCalculatorException("Can't add something that is not a number");
-		}
-		
 		return result;
 	}
 	
