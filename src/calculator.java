ef4726d479ea12a94477bb0be6f81cde34301127
steffe93 <grundstrom.stefan93@gmail.com>
2017-10-05 14:49:59 +0300

    exercise #4

--- src/calculator.java
+++ src/calculator.java
@@ -1,33 +1,34 @@
 import java.io.*;
 import java.util.Scanner;
+import java.util.regex.*;
 public class calculator {
 public static void main(String []args){
-	Scanner reader = new Scanner(System.in);
+	//Scanner reader = new Scanner(System.in);
+	Scanner number = new Scanner(System.in);
+	
 	int nextnum = 0;
-	int total = 0;
-	/*String line;
-	String[] lineVector;
-	int i;*/
+	int sumTotal = 0;
+	int totalNum = 0;
+
 	
 	while (nextnum >=0) {
 	System.out.println("Enter a number between 0-2: ");
-	int num = reader.nextInt();
-	/*line = reader.nextLine();
-	lineVector = line.split(",");
+	//int num = reader.nextInt();
+	String userNum = number.nextLine();
+	userNum = userNum.replaceAll("[/,]", "");
+	//String userNumX = userNum;
+	totalNum = Integer.parseInt(userNum);
 	
-	i = Integer.parseInt(lineVector[0]);*/
-	
-	if (num<=2 && num>=0) {
-		total = total + num;
-		nextnum++;
+
+	if(totalNum<=12 && totalNum>=0) {
+		sumTotal = sumTotal + totalNum;
+		System.out.println(sumTotal);
 	}
-	
 	else {
-		total = total;
+		System.out.println(sumTotal);
 		nextnum = -1;
 	}
-	
-	System.out.println(total);
+
 	}
 	
 }
