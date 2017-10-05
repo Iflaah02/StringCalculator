ba715acc0dccc6e19f674cb02e234e5663e1557b
jennitormala <tormalajenni@gmail.com>
2017-10-05 13:11:49 +0300

    finish refactoring

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,35 +1,18 @@
-import static org.junit.Assert.fail;
 
 public class StringCalculator {
 
 	public int add(String numbersStr) throws StringCalculatorException {
-
 		int sum = (numbersStr == "") ? 0 : calculateSum(numbersStr);
 		return sum;
 	}
 
-	private int calculateSum(String numbersStr) throws StringCalculatorException {
-
-		int sum = 0;
-
-		String number[] = numbersStr.split("[, || \\n]");
-		checkInputLimit(number.length);
-
-			for (int i = 0; i < number.length || i == 0; i++) {
-				int value = stringToNumber(number[i]);
-				sum += value;	
-		}
-			
-		return sum;
-	}
-
-	private void checkInputLimit(int length) throws StringCalculatorException {
+	private void validateLength(int length) throws StringCalculatorException {
 		if (length > 2) {
 			throw new StringCalculatorException();
 		}
 	}
 
-	private int stringToNumber(String number) throws StringCalculatorException {
+	private int validateNumber(String number) throws StringCalculatorException {
 		int value = 0;
 		try {
 			value = Integer.parseInt(number);
@@ -39,4 +22,29 @@ public class StringCalculator {
 		return value;
 	}
 
+	private String[] splitNumbers(String numberString) throws StringCalculatorException {
+		String numbers[] = numberString.split("[, || \\n]");
+		return numbers;
+	}
+
+	private int stringToNumber(String numberString) throws StringCalculatorException {
+		int number = validateNumber(numberString);
+		return number;
+	}
+
+	private int sumUpArray(String[] numberArray) throws StringCalculatorException {
+		int sum = 0;
+		for (int i = 0; i < numberArray.length || i == 0; i++) {
+			int value = stringToNumber(numberArray[i]);
+			sum += value;
+		}
+		return sum;
+	}
+
+	private int calculateSum(String numbersStr) throws StringCalculatorException {
+		String numbers[] = splitNumbers(numbersStr);
+		validateLength(numbers.length);
+		int sum = sumUpArray(numbers);
+		return sum;
+	}
 }
