269859cd727415c433f9d42094cdc0eb3295a273
arussell <arussell@ts137-pc16.student.yo.oulu.fi>
2017-10-05 14:54:04 +0300

    In progress.

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,9 +1,28 @@
 
 public class StringCalculator {
-	public int add(String numbersStr) {
-		// Returns the sum of the numbers given in numbersStr
-		
-		// not yet implemented
-		return 0;
+	public static void main (String[] args) {
+	String numbers = "1,2";
+	 int add(string numbers);
+	 
+	
+	
+	// public int add(String numbersStr) {
+		/* split the String numbersStr */
+		// String[] stringArray = numbersStr.split(" ");
+		// int sum=0;
+		// int temp=0;
+		// for (int i=0;i<stringArray.length;i++){
+		//	try {
+				/*Convert the numbers in numbersStr to int*/
+		//		temp = Integer.parseInt(stringArray[i]);
+		//		sum += temp;
+		//		}
+		//	catch(Exception e) {
+				
+		//	}
+		//	}
+		//return sum;
+		//
+	
 	}
 }
