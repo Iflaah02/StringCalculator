70c6792ef3056698051b85467e88e0671b338478
Joni Pennala <joni.pennala@gmail.com>
2017-10-20 08:47:25 +0300

    Assignment Finished

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,9 +1,66 @@
+import java.util.Arrays;
 
 public class StringCalculator {
-	public int add(String numbersStr) {
+	
+	private int firstNumber;
+	private int secondNumber;
+	
+	public int add(String numbersStr) throws StringCalculatorException{
 		// Returns the sum of the numbers given in numbersStr
-		
-		// not yet implemented
+		if (!numbersStr.isEmpty()){
+			try {
+				String [] strValues = parseString(numbersStr);
+				if  (strValues.length == 1) {
+					firstNumber = Integer.parseInt(strValues[0]);
+					return firstNumber;
+				} else if (strValues.length == 2) {
+					firstNumber = Integer.parseInt(strValues[0]);
+					secondNumber = Integer.parseInt(strValues[1]);
+					return firstNumber + secondNumber;
+				} else if (strValues.length > 2) {
+					int [] numValues = parseStringToNumbers(strValues);
+					int result = 0;
+					for (int i = 0; i < numValues.length; i++) {
+						result += numValues[i];
+					}
+					return result;
+				}
+			} catch (NumberFormatException e) {
+				throw new StringCalculatorException();
+			}
+			
+		}
 		return 0;
 	}
+
+	private int[] parseStringToNumbers(String [] stringArray) throws StringCalculatorException{
+		int [] numbers = new int[stringArray.length];
+		for (int i = 0; i < stringArray.length; i++) {
+			//if (numbers [i] >= 0) {
+			numbers [i] = Integer.parseInt(stringArray[i]);
+			//}
+			//else 
+				//throw new StringCalculatorException();
+		}
+		return numbers;
+	}
+	
+	private String [] parseString(String string) throws StringCalculatorException {
+		String [] values;
+		values = string.split("(,)|(\n)");
+		boolean stringIsValid = false;
+		for (int i = 0; i < values.length; i++) {
+			if (values[i].matches("^[0-9]+$")) {
+				stringIsValid = true;
+			} else {
+				stringIsValid = false;
+				break;
+			}
+		}
+		if (!stringIsValid)
+			throw new StringCalculatorException();
+		else 
+			return values;
+	}
+
 }
--- tests/StringCalculatorTest.java
+++ tests/StringCalculatorTest.java
@@ -5,8 +5,117 @@ import org.junit.Test;
 public class StringCalculatorTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test_add_emptyValues() throws StringCalculatorException {
+		//Arrange
+		String values = "";
+		StringCalculator strCalculator = new StringCalculator();
+		
+		//Act
+		int result = strCalculator.add(values);
+		// Assert
+		assertEquals("Result incorrect", 0, result);
 	}
+	
+	@Test
+	public void test_add_oneValue() throws StringCalculatorException {
+		//Arrange
+		String values = "1";
+		StringCalculator strCalculator = new StringCalculator();
+		
+		//Act
+		int result = strCalculator.add(values);
+		// Assert
+		assertEquals("Result incorrect", 1, result);
+	}
+	
+	@Test
+	public void test_add_twoValues() throws StringCalculatorException {
+		//Arrange
+		String values = "1,10";
+		StringCalculator strCalculator = new StringCalculator();
+		
+		//Act
+		int result = strCalculator.add(values);
+		// Assert
+		assertEquals("Result incorrect", 11, result);
+	}
+	
+	@Test
+	public void test_add_moreThanTwoValues() throws StringCalculatorException {
+		//Arrange
+		String values = "1,10,4,5,7,8";
+		StringCalculator strCalculator = new StringCalculator();
+		
+		//Act
+		int result = strCalculator.add(values);
+		// Assert
+		assertEquals("Result incorrect", 35, result);
+	}
+	
+	@Test
+	public void test_add_string_with_newline() throws StringCalculatorException {
+		//Arrange
+		String values = "1\n10\n4\n5\n7\n8";
+		StringCalculator strCalculator = new StringCalculator();
+		
+		//Act
+		int result = strCalculator.add(values);
+		// Assert
+		assertEquals("Result incorrect", 35, result);
+	}
+	
+	@Test
+	public void test_add_string_with_newline_and_comma() throws StringCalculatorException {
+		//Arrange
+		String values = "1\n10\n4,5\n7,8";
+		StringCalculator strCalculator = new StringCalculator();
+		
+		//Act
+		int result = strCalculator.add(values);
+		// Assert
+		assertEquals("Result incorrect", 35, result);
+	}
+	
+	@Test  (expected = StringCalculatorException.class)
+	public void test_add_negative_numbers() throws StringCalculatorException {
+		//Arrange
+		String values = "1\n10\n4,5\n7,-8";
+		StringCalculator strCalculator = new StringCalculator();
+		
+		//Act
+		int result = strCalculator.add(values);
+	}
+	
+	@Test  (expected = StringCalculatorException.class)
+	public void test_add_invalid_input() throws StringCalculatorException {
+		//Arrange
+		String values = "\n\n,\n";
+		StringCalculator strCalculator = new StringCalculator();
+		
+		//Act
+		int result = strCalculator.add(values);
+	}
+	
+	@Test  (expected = StringCalculatorException.class)
+	public void test_add_invalid_input2() throws StringCalculatorException {
+		//Arrange
+		String values = "n";
+		StringCalculator strCalculator = new StringCalculator();
+		
+		//Act
+		int result = strCalculator.add(values);
+	}
+	
+	@Test  (expected = StringCalculatorException.class)
+	public void test_add_invalid_input3() throws StringCalculatorException {
+		//Arrange
+		String values = ",";
+		StringCalculator strCalculator = new StringCalculator();
+		
+		//Act
+		int result = strCalculator.add(values);
+	}
+	
+	
 
 }
