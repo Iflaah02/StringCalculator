ffb84579597800d1e874747c62195ff99621f9c5
ilundbom <ilundbom@W10-116.student.yo.oulu.fi>
2018-09-26 17:27:13 +0300

    Works already pretty fine.

--- tests/StringCalculatorTest.java
+++ tests/StringCalculatorTest.java
@@ -6,7 +6,39 @@ public class StringCalculatorTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		StringCalculator calc = new StringCalculator();
+		
+		calc.add("5,2");
+		int sum = calc.returnSum();
+		assertEquals("Sum 5+2", 7, sum);
+	}
+	
+	@Test
+	public void test_3_numbers() {
+		StringCalculator calc = new StringCalculator();
+		
+		calc.add("1,2,3");
+		int sum2 = calc.returnSum();
+		assertEquals("Sum 1+2+3", 6, sum2);		
 	}
 
+	@Test
+	public void test_3_numbers_delimiters() {
+		StringCalculator calc = new StringCalculator();
+		
+		String testWord = calc.replacer("5\n5");
+		
+		assertEquals("5\n5", "5,5", testWord);
+		
+	}	
+	
+	@Test
+	public void test_3_numbers_different_delimiters() {
+		StringCalculator calc = new StringCalculator();
+		
+		calc.add("1\n2,3");
+		int sum2 = calc.returnSum();
+		assertEquals("Sum 1+2+3", 6, sum2);		
+	}	
+	
 }
