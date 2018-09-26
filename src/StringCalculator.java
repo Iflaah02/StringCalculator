2cd88bfff255f790ad26c82c54d5456861e6be48
papyy <papyy@W10-42.student.yo.oulu.fi>
2018-09-26 18:58:29 +0300

    Work in progress

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,9 +1,52 @@
 
 public class StringCalculator {
+	
+	
 	public int add(String numbersStr) {
 		// Returns the sum of the numbers given in numbersStr
-		
+		boolean lengthCheck = checkLength(numbersStr);
+		int sum = 0;
+		if (lengthCheck = true) {
+			String[] strArray = numbersStr.split(",|\\\n");
+			int[] intArray = new int[strArray.length];
+			for(int i = 0; i < strArray.length; i++) {
+			    intArray[i] = Integer.parseInt(strArray[i]);
+			}
+			
+			for (int j = 0; j < strArray.length; j++ ) {
+				sum = sum + intArray[j];
+			}
+		} 
 		// not yet implemented
-		return 0;
+		return sum;
 	}
+	
+	public boolean checkLength (String numbersStr) {
+		int stringLength = numbersStr.length();
+		
+		if (stringLength <= 0) {
+			return false;
+		} else {
+			String[] strArray = numbersStr.split(",|\\\n");
+			int[] intArray = new int[strArray.length];
+			for(int i = 0; i < strArray.length; i++) {
+			    intArray[i] = Integer.parseInt(strArray[i]);
+			}
+			if (strArray.length > 1) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public boolean checkInput (String numbersStr) {
+		String[] strArray = numbersStr.split(",|\\\n");
+		
+		if (numbersStr.matches("^[0-9,\n]+$")) {
+			return true;
+		} 
+		
+		return false;
+	}
+	
 }
--- tests/StringCalculatorTest.java
+++ tests/StringCalculatorTest.java
@@ -5,8 +5,56 @@ import org.junit.Test;
 public class StringCalculatorTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testStringLength0() {
+		
+		StringCalculator calculator = new StringCalculator();
+		String numbersStr = "";
+		
+		assertEquals(false , calculator.checkLength(numbersStr));
+	}
+	
+	@Test
+	public void testStringLength3() {
+		
+		StringCalculator calculator = new StringCalculator();
+		String numbersStr = "1,2,3";
+		
+		assertEquals(false , calculator.checkLength(numbersStr));
+	}
+	
+	@Test
+	public void testSum() {
+		
+		StringCalculator calculator = new StringCalculator();
+		String numbersStr = "1,2";
+		
+		assertEquals(3 , calculator.add(numbersStr));
+	}
+	
+	@Test
+	public void testSum3() {
+		
+		StringCalculator calculator = new StringCalculator();
+		String numbersStr = "1\n2";
+		
+		assertEquals(3 , calculator.add(numbersStr));
 	}
 
+	@Test
+	public void testLetters() throws StringCalculatorException{
+		
+		StringCalculator calculator = new StringCalculator();
+		String numbersStr = "abc";
+		
+		assertEquals(false , calculator.checkInput(numbersStr));
+	}
+	
+	@Test
+	public void testLetters2() throws StringCalculatorException{
+		
+		StringCalculator calculator = new StringCalculator();
+		String numbersStr = "n12";
+		
+		assertEquals(false , calculator.checkInput(numbersStr));
+	}
 }
