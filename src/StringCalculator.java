d3b543384f95f1ba0998052684429ab3952ac233
swongsta18 <swongsta18@W10-144.student.yo.oulu.fi>
2018-09-26 18:45:04 +0300

    First Commit

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,9 +1,46 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class StringCalculator {
-	public int add(String numbersStr) {
+	public int add(String numbersStr) throws StringCalculatorException {
 		// Returns the sum of the numbers given in numbersStr
 		
-		// not yet implemented
-		return 0;
+		//get the numbers
+		ArrayList<Integer> intArray = getArrayInteger(numbersStr);
+		
+		//get the sum
+		int result = calculateSum(intArray);
+		
+		return result;
+	}
+		
+	public ArrayList<Integer> getArrayInteger(String str) throws StringCalculatorException {
+		//split with comma or newline
+		String[] splitedStr = str.split(",|\\n", -1);
+		ArrayList<Integer> intArray = new ArrayList<Integer>();
+		int temp;
+		for (int i=0;i<splitedStr.length;i++) {
+			try {
+				temp = Integer.parseInt(splitedStr[i]);
+			} catch(NumberFormatException e) {
+				throw new StringCalculatorException();
+			}
+			if (temp < 0) {
+				//negative number
+				throw new StringCalculatorException();
+			}
+			else {
+				intArray.add(temp);
+			}
+		}
+		return intArray;
+	}
+	
+	public int calculateSum(ArrayList<Integer> intArray) {
+		int result = 0;
+		for (int i=0; i<intArray.size(); i++) {
+			result += intArray.get(i);
+		}
+		return result;
 	}
 }
--- tests/StringCalculatorTest.java
+++ tests/StringCalculatorTest.java
@@ -1,12 +1,120 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import org.junit.Test;
 
 public class StringCalculatorTest {
-
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test_getArrayInteger_13_14_comma() throws StringCalculatorException {
+		//Arrange
+		StringCalculator calculator = new StringCalculator();
+		String str = "13,14";
+		
+		Integer[] res = new Integer[] {13, 14};
+		ArrayList<Integer> expected = new ArrayList<Integer>(Arrays.asList(res));
+		
+		//Act
+		ArrayList<Integer> intArray = calculator.getArrayInteger(str);
+		
+		//Assert
+		assertEquals(expected ,intArray);
+	}
+	
+	@Test
+	public void test_getArrayInteger_8_9_newline() throws StringCalculatorException {
+		//Arrange
+		StringCalculator calculator = new StringCalculator();
+		String str = "8\n9";
+		
+		Integer[] res = new Integer[] {8, 9};
+		ArrayList<Integer> expected = new ArrayList<Integer>(Arrays.asList(res));
+		
+		//Act
+		ArrayList<Integer> intArray = calculator.getArrayInteger(str);
+		
+		//Assert
+		assertEquals(expected ,intArray);
+	}
+	
+	@Test
+	public void test_getArrayInteger_3_12_28_mix() throws StringCalculatorException {
+		//Arrange
+		StringCalculator calculator = new StringCalculator();
+		String str = "3\n12,28";
+		
+		Integer[] res = new Integer[] {3, 12, 28};
+		ArrayList<Integer> expected = new ArrayList<Integer>(Arrays.asList(res));
+		
+		//Act
+		ArrayList<Integer> intArray = calculator.getArrayInteger(str);
+		
+		//Assert
+		assertEquals(expected ,intArray);
+	}
+	
+	@Test (expected = StringCalculatorException.class)
+	public void test_getArrayInteger_throw_exception_notnumber() throws StringCalculatorException {
+		//Arrange
+		StringCalculator calculator = new StringCalculator();
+		String str = "3.14\n12,28";
+		
+		//Act - should cause exception
+		ArrayList<Integer> intArray = calculator.getArrayInteger(str);
+		
+	}
+	
+	@Test (expected = StringCalculatorException.class)
+	public void test_getArrayInteger_throw_exception_invaliddelimiter() throws StringCalculatorException {
+		//Arrange
+		StringCalculator calculator = new StringCalculator();
+		String str = "2,\n3";
+		
+		//Act - should cause exception
+		ArrayList<Integer> intArray = calculator.getArrayInteger(str);
+		
+	}
+	
+	@Test (expected = StringCalculatorException.class)
+	public void test_getArrayInteger_throw_exception_negative() throws StringCalculatorException {
+		//Arrange
+		StringCalculator calculator = new StringCalculator();
+		String str = "2,-7\n3";
+		
+		//Act - should cause exception
+		ArrayList<Integer> intArray = calculator.getArrayInteger(str);
+		
+	}
+	
+	@Test
+	public void test_CalculateSum_5_9() {
+		//Arrange
+		StringCalculator calculator = new StringCalculator();
+		
+		Integer[] input = new Integer[] {5, 9};
+		ArrayList<Integer> inputArray = new ArrayList<Integer>(Arrays.asList(input));
+		
+		//Act
+		int result = calculator.calculateSum(inputArray);
+		
+		//Assert
+		assertEquals(14 ,result);
+	}
+	
+	@Test
+	public void test_CalculateSum_3_12_28() {
+		//Arrange
+		StringCalculator calculator = new StringCalculator();
+		
+		Integer[] input = new Integer[] {3, 12, 28};
+		ArrayList<Integer> inputArray = new ArrayList<Integer>(Arrays.asList(input));
+		
+		//Act
+		int result = calculator.calculateSum(inputArray);
+		
+		//Assert
+		assertEquals(43 ,result);
 	}
 
 }
