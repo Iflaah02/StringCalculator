56b6d356e5459730172b32fd633e6ab0bf8ddc85
npelkone <niko.pelkonen@hotmail.com>
2018-09-26 20:02:49 +0300

    Assignment finished

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,9 +1,49 @@
+import java.util.ArrayList;
 
 public class StringCalculator {
-	public int add(String numbersStr) {
-		// Returns the sum of the numbers given in numbersStr
-		
-		// not yet implemented
-		return 0;
+	public boolean tooManyNumbers;
+	public int count = 0;
+	public int sum = 0;
+	ArrayList<String> strsArr = new ArrayList();
+	ArrayList<Integer> numsArr = new ArrayList();
+	StringCalculatorTest tests = new StringCalculatorTest();
+	
+	public static void main(String[] args) {
+		StringCalculator sc = new StringCalculator();
 	}
+	
+	public int add(String numbersStr) {
+		splitNumbers(numbersStr);
+		for (String i : strsArr) {
+			numsArr.add(Integer.parseInt(i));
+		}
+		for (int num : numsArr) {
+			sum += num;
+		}
+		return sum;
+	}
+	
+	
+	public void splitNumbers(String numbers) {
+		for (String s : numbers.split(",|\\n")) {
+			if (s == "") {
+				break;
+			}
+			else {
+				checkCount(s);
+			}
+		}
+	}
+	
+	public boolean checkCount(String s) {
+		count++;
+		if (count < 4) {
+			strsArr.add(s);
+			return false;
+		}
+		else {
+			return true;
+		}
+	}
+	
 }
--- tests/StringCalculatorTest.java
+++ tests/StringCalculatorTest.java
@@ -4,9 +4,83 @@ import org.junit.Test;
 
 public class StringCalculatorTest {
 
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test_sum_6() {
+		StringCalculator sc = new StringCalculator();
+		assertEquals(sc.add("1,2,3"), 6);
+	}
+	
+	@Test
+	public void test_one_value() {
+		StringCalculator sc = new StringCalculator();
+		assertEquals(sc.add("2"), 2);
+	}
+	
+	@Test
+	public void test_empty() {
+		StringCalculator sc = new StringCalculator();
+		assertEquals(sc.add(""), 0);
+	}
+	
+	@Test
+	public void test_four_commas() {
+		StringCalculator sc = new StringCalculator();
+		assertEquals(sc.add("2,4,6,8"), 12);
+	}
+	
+	@Test
+	public void test_three_commas() {
+		StringCalculator sc = new StringCalculator();
+		assertEquals(sc.add("2,4,1"), 7);
+	}
+	
+	@Test
+	public void test_two_commas() {
+		StringCalculator sc = new StringCalculator();
+		assertEquals(sc.add("2,6"), 8);
+	}
+	
+	@Test
+	public void test_four_newlines() {
+		StringCalculator sc = new StringCalculator();
+		assertEquals(sc.add("2\n4\n6\n8"), 12);
+	}
+	
+	@Test
+	public void test_three_newlines() {
+		StringCalculator sc = new StringCalculator();
+		assertEquals(sc.add("2\n4\n1"), 7);
+	}
+	
+	@Test
+	public void test_two_newlines() {
+		StringCalculator sc = new StringCalculator();
+		assertEquals(sc.add("2\n6"), 8);
+	}
+	
+	@Test
+	public void test_one_comma_two_newlines() {
+		StringCalculator sc = new StringCalculator();
+		assertEquals(sc.add("2,3\n6\n5"), 11);
+	}
+	
+	@Test
+	public void test_two_comma_one_newlines() {
+		StringCalculator sc = new StringCalculator();
+		assertEquals(sc.add("2\n3,6,5"), 11);
+	}
+	
+	@Test
+	public void test_one_comma_one_newline() {
+		StringCalculator sc = new StringCalculator();
+		assertEquals(sc.add("2\n3,6"), 11);
+	}
+	
+	@Test
+	public void test_check_count_false() {
+		StringCalculator sc = new StringCalculator();
+		assertEquals(sc.checkCount("3"),false);
 	}
 
 }
