5af5118106eb524bd28903e0431292e2b5eedb4f
tkurtti <tkurtti@W10-191.student.yo.oulu.fi>
2018-09-27 14:43:48 +0300

    Implement slice 1

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -3,7 +3,27 @@ public class StringCalculator {
 	public int add(String numbersStr) {
 		// Returns the sum of the numbers given in numbersStr
 		
-		// not yet implemented
-		return 0;
+		if (numbersStr.equals("")) {
+			return 0;
+		}
+		
+		String[] splits = numbersStr.split(",");
+		int sum = 0;
+		
+		System.out.println(splits.length);
+		
+		if (splits.length > 0) {
+			sum += Integer.parseInt(splits[0]);
+		} 
+		
+		if (splits.length > 1) {
+			sum += Integer.parseInt(splits[1]);
+		} 
+		
+		if (splits.length > 2) {
+			sum += Integer.parseInt(splits[2]);
+		} 
+		
+		return sum;
 	}
 }
--- tests/StringCalculatorTest.java
+++ tests/StringCalculatorTest.java
@@ -5,8 +5,37 @@ import org.junit.Test;
 public class StringCalculatorTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test_emptyString_returnsZero() {
+		StringCalculator calc = new StringCalculator();
+		int result = calc.add("");
+		assertEquals(0, result);
+	}
+	
+	@Test
+	public void test_singleDigitString_returnsParsedNumber() {
+		StringCalculator calc = new StringCalculator();
+		int result = calc.add("1");
+		assertEquals(1, result);
 	}
 
+	@Test
+	public void test_multiDigitString_returnsParsedNumber() {
+		StringCalculator calc = new StringCalculator();
+		int result = calc.add("599599");
+		assertEquals(599599, result);
+	}
+	
+	@Test
+	public void test_twoNumbersByComma_returnsTheirSum() {
+		StringCalculator calc = new StringCalculator();
+		int result = calc.add("2,3");
+		assertEquals(5, result);
+	}
+	
+	@Test
+	public void test_threeNumbersByComma_returnsTheirSum() {
+		StringCalculator calc = new StringCalculator();
+		int result = calc.add("3,4,5");
+		assertEquals(12, result);
+	}
 }
