71f3ce44d4bd73e8d4f8fab094f597c83196b00d
Soletek <tuukkak123@gmail.com>
2018-09-27 15:08:30 +0300

    Implement slice 2

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,28 +1,40 @@
-
 public class StringCalculator {
 	public int add(String numbersStr) {
 		// Returns the sum of the numbers given in numbersStr
 		
-		if (numbersStr.equals("")) {
+		if (!validateString(numbersStr)) {
 			return 0;
 		}
 		
-		String[] splits = numbersStr.split(",");
+		String[] strings = numbersStr.split(",");
+		int[] parsedNumbers = parseStringsToIntegers(strings);
+		int sum = sumNumbers(parsedNumbers);
+		
+		return sum;
+	}
+	
+	public boolean validateString(String numbersStr) {
+		if (numbersStr.length() == 0) return false;
+		
+		return true;
+	}
+	
+	public int[] parseStringsToIntegers(String[] numberStrings) {
+		int[] parsedNumbers = new int[numberStrings.length];
+		
+		for (int i = 0; i < numberStrings.length; i++) {
+			parsedNumbers[i] = Integer.parseInt(numberStrings[i]);
+		}
+		
+		return parsedNumbers;
+	}
+	
+	public int sumNumbers(int[] numbers) {
 		int sum = 0;
 		
-		System.out.println(splits.length);
-		
-		if (splits.length > 0) {
-			sum += Integer.parseInt(splits[0]);
-		} 
-		
-		if (splits.length > 1) {
-			sum += Integer.parseInt(splits[1]);
-		} 
-		
-		if (splits.length > 2) {
-			sum += Integer.parseInt(splits[2]);
-		} 
+		for (int i = 0; i < numbers.length; i++) {
+			sum += numbers[i];
+		}
 		
 		return sum;
 	}
--- tests/StringCalculatorTest.java
+++ tests/StringCalculatorTest.java
@@ -3,7 +3,8 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class StringCalculatorTest {
-
+	// Requirement 1:
+	
 	@Test
 	public void test_emptyString_returnsZero() {
 		StringCalculator calc = new StringCalculator();
@@ -37,5 +38,63 @@ public class StringCalculatorTest {
 		StringCalculator calc = new StringCalculator();
 		int result = calc.add("3,4,5");
 		assertEquals(12, result);
+	}
+	
+	// Requirement 2:
+	
+	@Test
+	public void test_add_fiveNumbersByComma_returnsTheirSum() {
+		StringCalculator calc = new StringCalculator();
+		int result = calc.add("5,4,3,2,1");
+		assertEquals(15, result);
+	}
+	
+	@Test
+	public void test_add_tenNumbersByComma_returnsTheirSum() {
+		StringCalculator calc = new StringCalculator();
+		int result = calc.add("1,10,100,1000,10000,1,10,100,1000,10000");
+		assertEquals(22222, result);
+	}
+	
+	@Test
+	public void test_sumNumbers_retunsZeroFromEmptyList() {
+		StringCalculator calc = new StringCalculator();
+		int[] numbers = new int[0];
+		int result = calc.sumNumbers(numbers);
+		assertEquals(0, result);
+	}
+	
+	@Test
+	public void test_sumNumbers_singleIntegerReturnsSameValue() {
+		StringCalculator calc = new StringCalculator();
+		int[] numbers = {5000};
+		int result = calc.sumNumbers(numbers);
+		assertEquals(5000, result);
+	}
+	
+	@Test
+	public void test_sumNumbers_sumsThreeIntegersCorrectly() {
+		StringCalculator calc = new StringCalculator();
+		int[] numbers = {3, 5, -4};
+		int result = calc.sumNumbers(numbers);
+		assertEquals(4, result);
+	}
+	
+	@Test
+	public void test_parseStringsToIntegers_emptyList() {
+		StringCalculator calc = new StringCalculator();
+		String[] strings = new String[0];
+		int[] result = calc.parseStringsToIntegers(strings);
+		assertEquals(0, result.length);
+	}
+	
+	@Test
+	public void test_parseStringsToIntegers_parsesIntegersCorrectly() {
+		StringCalculator calc = new StringCalculator();
+		String[] strings = {"10", "25", "3"};
+		int[] result = calc.parseStringsToIntegers(strings);
+		assertEquals(10, result[0]);
+		assertEquals(25, result[1]);
+		assertEquals(3, result[2]);
 	}
 }
