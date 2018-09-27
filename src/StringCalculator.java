afa7c599a2d1b329f9b018ab8641efb6e1b8aade
veeti <veetivimpari@gmail.com>
2018-09-27 20:36:49 +0300

    Assignment finished

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -2,8 +2,45 @@
 public class StringCalculator {
 	public int add(String numbersStr) {
 		// Returns the sum of the numbers given in numbersStr
+		int sum = 0;
+		int numbersInt = 0;
 		
-		// not yet implemented
-		return 0;
+		numbersStr = format(numbersStr);
+
+		if (validate(numbersStr)) {
+			try {
+				numbersInt = Integer.parseInt(numbersStr);
+			} catch(NumberFormatException | AssertionError e) {
+				System.out.println("Invalid input");
+				return 0;
+			}
+			while (numbersInt > 0) {
+				sum = sum + numbersInt % 10;
+				numbersInt = numbersInt / 10;
+			}
+		return sum;
+		} else {
+			return 0;
+		}
 	}
+	
+	public boolean validate(String numbersStr) {
+		boolean stringIsValid = true;
+		
+		if (numbersStr.matches("[0-9]+") == false) {
+			stringIsValid = false;
+		} else if (numbersStr.length() == 0) {
+			stringIsValid = false;
+		}
+		
+		return stringIsValid;
+	}
+	
+	public String format(String numbersStr) {
+		// get rid of the columns and spaces and such
+		numbersStr = numbersStr.replaceAll(",", "");
+		numbersStr = numbersStr.replaceAll("\\s+","");
+		return numbersStr;
+	}
+	
 }
--- tests/StringCalculatorTest.java
+++ tests/StringCalculatorTest.java
@@ -5,8 +5,51 @@ import org.junit.Test;
 public class StringCalculatorTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testStringCalculator_EmptyStringShouldBe0() {
+		StringCalculator stringCalculator = new StringCalculator();
+		int sum = stringCalculator.add("");
+		assertEquals("empty string should return 0", sum, 0);
 	}
-
+	
+	@Test
+	public void testStringCalculator_OneNumberShouldReturnItself() {
+		StringCalculator stringCalculator = new StringCalculator();
+		int sum = stringCalculator.add("1");
+		assertEquals("should result the same but integer", sum, 1);
+	}
+	
+	@Test
+	public void testStringCalculator_addTwoNumbers() {
+		StringCalculator stringCalculator = new StringCalculator();
+		int sum = stringCalculator.add("13");
+		assertEquals("string '13' should become 1+3=4", sum, 4);
+	}
+	
+	@Test
+	public void testStringCalculator_addTwoNumbersWithDot() {
+		StringCalculator stringCalculator = new StringCalculator();
+		int sum = stringCalculator.add("1,3");
+		assertEquals("string '13' should become 1+3=4", sum, 4);
+	}
+	
+	@Test
+	public void testStringCalculator_addTwoNumbersSeparatedWithSpace() {
+		StringCalculator stringCalculator = new StringCalculator();
+		int sum = stringCalculator.add("1 3");
+		assertEquals("string '13' should become 1+3=4", sum, 4);
+	}
+	
+	@Test
+	public void testStringCalculator_handleInvalidInput() {
+		StringCalculator stringCalculator = new StringCalculator();
+		int sum = stringCalculator.add("asdads");
+		assertEquals("invalid input should return 0", sum, 0);
+	}
+	
+	@Test
+	public void testStringCalculator_handleSpecialCharacters() {
+		StringCalculator stringCalculator = new StringCalculator();
+		int sum = stringCalculator.add("$¤#?");
+		assertEquals("invalid input should return 0", sum, 0);
+	}
 }
