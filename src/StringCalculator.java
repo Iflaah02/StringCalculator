c11c7a530bb3c0b46d0b8f8000cad2786ad15201
pahayryn <pahayryn@W10-167.student.yo.oulu.fi>
2018-09-27 16:20:19 +0300

    Not finish

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,9 +1,48 @@
-
 public class StringCalculator {
 	public int add(String numbersStr) {
 		// Returns the sum of the numbers given in numbersStr
+		int summary = HandleUnknownAmountOfNumber(numbersStr);
+
+		return summary;
+	}
+
+	public int HandleUnknownAmountOfNumber (String numbersStr) {
+		int StrLength = numbersStr.length();
 		
-		// not yet implemented
+		if (StrLength == 0) {
+			return 0;
+		}
+		
+		else if (StrLength > 0) {
+			int summary = HandleNewLinesBetweenNumbers(numbersStr);
+			return summary;
+		}
 		return 0;
+	}
+	
+	public int HandleNewLinesBetweenNumbers(String numbersStr) {
+		
+		String[] numbers = numbersStr.split("\n" + ",");
+		int summary = 0;
+
+		int numLength = numbers.length;
+		while (numLength != 0){
+			try {
+				int number = Integer.parseInt(numbers[0]);
+				
+				if (number < 0) {
+					Exception exc = new StringCalculatorException();
+				}
+				else {
+					summary = summary + number;				
+				}
+			}
+			catch (NumberFormatException e) {
+				Exception exc = new StringCalculatorException();
+			}
+			numLength = numLength -1;
+		}
+		
+		return summary;
 	}
 }
