789f4360f5ec462f9f1c29d88e6ba900cd3a4342
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2018-09-27 15:06:35 +0300

    assignment finished

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,9 +1,51 @@
 
 public class StringCalculator {
-	public int add(String numbersStr) {
+	public int add(String numbersStr) throws StringCalculatorException {
 		// Returns the sum of the numbers given in numbersStr
-		
-		// not yet implemented
-		return 0;
-	}
+		if(numbersStr.isEmpty())
+		    return 0;
+
+        int sum = getSum(numbersStr);
+
+        return sum;
+    }
+
+    private int getSum(String numbersStr) throws StringCalculatorException {
+        final String[] splitStr = splitInput(numbersStr);
+
+        int sum = 0;
+
+        for(int i = 0; i < splitStr.length; ++i){
+
+            final int currentNumber  = verifyNumber(splitStr[i]);
+
+            sum += currentNumber;
+        }
+
+        return sum;
+    }
+
+    private String[] splitInput(String numbersStr) {
+        return numbersStr.split("[,\n]", -1);
+    }
+
+    /**
+     * Checks that input string number is valid
+     * @param numberStr
+     * @return
+     * @throws StringCalculatorException
+     */
+    private int verifyNumber(String numberStr) throws StringCalculatorException {
+	    try {
+            final int number = Integer.parseInt(numberStr);
+
+            if(number < 0)
+                throw new StringCalculatorException();
+
+            return number;
+
+        } catch (NumberFormatException e){
+	        throw new StringCalculatorException();
+        }
+    }
 }
--- tests/StringCalculatorTest.java
+++ tests/StringCalculatorTest.java
@@ -1,12 +1,110 @@
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
 
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class StringCalculatorTest {
+class StringCalculatorTest {
+    @Test
+    void addWorksWithEmptyString() throws StringCalculatorException {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+        StringCalculator calc = new StringCalculator();
+        assertEquals(0, calc.add(""));
+    }
 
+    @Test
+    void addWorksWithOneNumber() throws StringCalculatorException {
+
+        StringCalculator calc = new StringCalculator();
+        assertEquals(1, calc.add("1"));
+    }
+
+    @Test
+    void addWorksWithTwoNumbers() throws StringCalculatorException {
+
+        StringCalculator calc = new StringCalculator();
+        assertEquals(3, calc.add("1,2"));
+    }
+
+    @Test
+    void addWorksWithThreeNumbers() throws StringCalculatorException {
+
+        StringCalculator calc = new StringCalculator();
+        assertEquals(6, calc.add("1,2,3"));
+    }
+
+    @Test
+    void addWorksWithAnyNumberOfNumbers() throws StringCalculatorException {
+
+        int testTo = 62;
+
+        int sum = 0;
+
+        // Build test string
+        String str = "";
+
+        for(int i = 0; i < testTo; ++i){
+
+            if(i > 0)
+                str += ",";
+
+            str += Integer.toString(i);
+            sum += i;
+        }
+
+        StringCalculator calc = new StringCalculator();
+        assertEquals(sum, calc.add(str));
+    }
+
+    @Test
+    void addWorksWithNewLinesAsSeparators() throws StringCalculatorException {
+
+        StringCalculator calc = new StringCalculator();
+        assertEquals(6, calc.add("1\n2\n3"));
+    }
+
+    @Test
+    void addWorksWithNewLinesAndMixedCommasAsSeparators() throws StringCalculatorException {
+
+        StringCalculator calc = new StringCalculator();
+        assertEquals(6, calc.add("1\n2,3"));
+    }
+
+    @Test
+    void addThrowsIfEndingWithSeparator() throws StringCalculatorException {
+
+        StringCalculator calc = new StringCalculator();
+        assertThrows(StringCalculatorException.class,
+                () -> {
+            calc.add("1,2,");
+        });
+    }
+
+    @Test
+    void addThrowsIfEndingWithDoubleSeparator() throws StringCalculatorException {
+
+        StringCalculator calc = new StringCalculator();
+        assertThrows(StringCalculatorException.class,
+                () -> {
+                    calc.add("1,2,\n");
+                });
+    }
+
+    @Test
+    void addThrowsWithNegativeNumbers() throws StringCalculatorException {
+
+        StringCalculator calc = new StringCalculator();
+        assertThrows(StringCalculatorException.class,
+                () -> {
+                    calc.add("1,2,-3");
+                });
+    }
+
+    @Test
+    void addThrowsWithNotNumbers() throws StringCalculatorException {
+
+        StringCalculator calc = new StringCalculator();
+        assertThrows(StringCalculatorException.class,
+                () -> {
+                    calc.add("1,2,a");
+                });
+    }
 }
