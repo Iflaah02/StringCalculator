e81a670638c7ea6c01c8475e3f436b855b6933b8
Bagel <Bagel@DESKTOP-2A5BCQ4>
2018-09-27 19:16:36 +0300

    Assignment Finnished

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,9 +1,46 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class StringCalculator {
-	public int add(String numbersStr) {
-		// Returns the sum of the numbers given in numbersStr
-		
-		// not yet implemented
-		return 0;
+	
+	public int add(String numberStr) throws StringCalculatorException {
+		int result = 0;
+		String splitStr = numberStr;
+		String[] arrSplit = splitStr.split(",");
+
+		if (numberStr == "") {
+			return 0;
+		}
+		if (arrSplit.length == 1) {
+			return Integer.parseInt(arrSplit[0]);
+		}
+
+		result = (handleNum(numberStr).get(0)+handleNum(numberStr).get(1));
+		return result;
+	}
+	
+	public ArrayList<Integer> handleNum(String numberStr) throws StringCalculatorException {
+
+		String splitStr = numberStr;	
+		String[] arrSplit = splitStr.split(",");
+		if(arrSplit[0].matches("[A-Za-z]")) {
+			System.out.println("Not numeric");
+			throw new StringCalculatorException();
+		}
+		if (arrSplit.length > 2) {
+			System.out.println("Invalid string");
+			throw new StringCalculatorException();
+		}
+		if ( Integer.parseInt(arrSplit[0])  < 0 || Integer.parseInt(arrSplit[1]) < 0 ) {
+			System.out.println("Negative string");
+			throw new StringCalculatorException();
+		}
+		ArrayList <Integer> ints = new ArrayList<Integer>();
+		ints.add(Integer.parseInt(arrSplit[0]));
+		ints.add(Integer.parseInt(arrSplit[1]));
+		return ints;
 	}
 }
+
+
+
--- tests/StringCalculatorTest.java
+++ tests/StringCalculatorTest.java
@@ -3,10 +3,42 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class StringCalculatorTest {
-
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	
+	
+	
+	@Test (expected = StringCalculatorException.class)
+	public void handleNumTooMany() throws StringCalculatorException {
+		StringCalculator calc = new StringCalculator();
+		calc.handleNum("1,2,3");
+	}
+	
+	@Test (expected = StringCalculatorException.class)
+	public void handleNumNeg() throws StringCalculatorException {
+		StringCalculator calc = new StringCalculator();
+		calc.handleNum("-1,-2");
 	}
 
+	@Test 
+	public void addEmpty() throws StringCalculatorException {
+		StringCalculator calc = new StringCalculator();
+		assertEquals(0, calc.add(""));
+	}
+	
+	@Test 
+	public void add12_3() throws StringCalculatorException {
+		StringCalculator calc = new StringCalculator();
+		assertEquals(3, calc.add("1,2"));
+	}
+	
+	@Test 
+	public void addOne() throws StringCalculatorException {
+		StringCalculator calc = new StringCalculator();
+		assertEquals(1, calc.add("1"));
+	}
+	
+	@Test (expected = StringCalculatorException.class)
+	public void addAlpha() throws StringCalculatorException {
+		StringCalculator calc = new StringCalculator();
+		calc.add("a,a");
+	}
 }
