170a28f07f127e7dc3864c26c619459bd7c26a75
robroooh <robroo.pc@gmail.com>
2018-09-28 23:45:32 +0300

    need the tests

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,9 +1,51 @@
+import java.util.Arrays;
+import java.util.List;
 
 public class StringCalculator {
-	public int add(String numbersStr) {
+	String rex = "[,\n][,\n]";
+	
+	public boolean checkNumbersLength(List<String> numbers) {
+		if (numbers.size() > 2) {
+			return false;
+		}
+		return true;
+	}
+	
+	public List<String> handleNewlineComma(String numbersStr) throws StringCalculatorException {
+		if (numbersStr.matches(rex)) {
+			throw new StringCalculatorException();
+		}
+		return Arrays.asList(numbersStr.split(",|\n"));
+	}
+	
+	public int add(String numbersStr) throws StringCalculatorException {
 		// Returns the sum of the numbers given in numbersStr
+		if(numbersStr == null || numbersStr.isEmpty()) {
+			return 0;
+		}
 		
-		// not yet implemented
-		return 0;
+		List<String> numbers = handleNewlineComma(numbersStr);
+		
+		if (!checkNumbersLength(numbers)) {
+			return 0;
+		}
+
+		int sum = 0;
+		try {
+			
+			int intx;
+			for (String x : numbers) {
+				intx = Integer.parseInt(x);
+				if (intx < 0) {
+					throw new StringCalculatorException();
+				}
+				sum += intx;
+			}
+		} catch (NumberFormatException e){
+			throw new StringCalculatorException();
+		}
+		
+		
+		return sum;
 	}
 }
