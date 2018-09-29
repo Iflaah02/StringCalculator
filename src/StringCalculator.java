1c01dfa57e1cc9cf37ac7c06da4705b5486e7ff5
Paukku <38953125+Paukku@users.noreply.github.com>
2018-09-29 12:23:22 +0300

    Assigment finished

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,7 +1,10 @@
 public class StringCalculator {
+	private int helpnumnber;
+
 	public int add(String numbersStr) {
 		// Returns the sum of the numbers given in numbersStr
 		int summary = HandleUnknownAmountOfNumber(numbersStr);
+		System.out.println("Summary is " + summary);
 
 		return summary;
 	}
@@ -10,6 +13,7 @@ public class StringCalculator {
 		int StrLength = numbersStr.length();
 		
 		if (StrLength == 0) {
+			System.out.println("1");
 			return 0;
 		}
 		
@@ -21,28 +25,39 @@ public class StringCalculator {
 	}
 	
 	public int HandleNewLinesBetweenNumbers(String numbersStr) {
-		
-		String[] numbers = numbersStr.split("\n" + ",");
-		int summary = 0;
-
-		int numLength = numbers.length;
-		while (numLength != 0){
-			try {
-				int number = Integer.parseInt(numbers[0]);
-				
-				if (number < 0) {
-					Exception exc = new StringCalculatorException();
-				}
-				else {
-					summary = summary + number;				
-				}
-			}
-			catch (NumberFormatException e) {
-				Exception exc = new StringCalculatorException();
-			}
-			numLength = numLength -1;
+		 
+		if(numbersStr.matches("(.*),\n(.*)|(.*)\n,(.*)" )) {
+			Exception exc = new StringCalculatorException();
 		}
+		else {
+			String[] numbers = numbersStr.split("\n|\\,");
+			int helpnumber = 0;
+			int summary = 0;
+			int numLength = numbers.length;
+			
+			while (numLength != 0){
+				try {
+					int number = Integer.parseInt(numbers[helpnumber]);
+					
+					if (number < 0) {
+						Exception exc = new StringCalculatorException();
+						return 0;
+						
+					}
+					else {
+						summary = summary + number;				
+					}
+				}
+				catch (NumberFormatException e) {
+					Exception exc = new StringCalculatorException();
+					return 0;
+				}
+				numLength = numLength -1;
+				helpnumber = helpnumber +1;
+			}
 		
 		return summary;
+		}
+		return 0;
 	}
 }
