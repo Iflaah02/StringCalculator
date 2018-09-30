e391ec2a705674652605f303b94fc593db59b8fb
zhwu <zhi_hong.wu@yahoo.com>
2018-09-30 08:21:25 +0300

    Assignment Finished

--- tests/StringCalculatorTest.java
+++ tests/StringCalculatorTest.java
@@ -6,7 +6,43 @@ public class StringCalculatorTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		StringCalculator cal = new StringCalculator();
+		cal.add("");	
+		cal.add("2");		
+		cal.add("-2");		
+		cal.add("a");		
+		cal.add("1,2,3");		
+		cal.add("1,\n");	
+		cal.add("\n,1");	
+		cal.add("-1,a");
+		cal.add("a,-1");
+		
+		cal.add("1\n2,3");
+		cal.add("1\n2,3\n4");
+		cal.add("1,3\n4");
+		cal.add("1,2");
+
+		cal.add("a,2");
+		cal.add("-1,2");
+		cal.add("1,a");
+		cal.add("1,-2");
+				
+		cal.add("a\n2,3");		
+		cal.add("-1\n2,3");
+		cal.add("1\na,3");
+		cal.add("1\n-2,3");
+		cal.add("1,a\n4");
+		cal.add("1,-3\n4");
+		cal.add("1,3\na");
+		cal.add("1,3\n-4");
+
+		cal.add("1\n2,a");
+		cal.add("1\n2,-2");
+		cal.add("a,3\n4");
+		cal.add("-1,3\n4");
+		cal.add("a\nb,c\nd");
+		cal.add("-1\n-2,-3\n-4");
+		cal.add("-1\n\n,-3\n-4");
 	}
 
 }
