a3ba584f13d1ddab56dc090da2aa99fb7a43daa5
Tao Wenting <twtoulu@gmail.com>
2018-10-01 00:51:57 +0300

    Assignment	Finished

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,9 +1,51 @@
-
 public class StringCalculator {
-	public int add(String numbersStr) {
-		// Returns the sum of the numbers given in numbersStr
+	
+	public StringCalculator( ) {
 		
-		// not yet implemented
-		return 0;
+		
 	}
+	
+	public static int Add(String text) throws StringCalculatorException{
+		if(text.contains("\n")) {
+			text = text.replaceAll("\n",",").replace("\r", ",");
+		}
+		
+		if(text.equals("")){
+			return 0;
+		}
+		else if(text.contains("-"))
+			throw new StringCalculatorException();
+		else if(text.contains(",")){
+			return sum(splitNumbers(text));	
+		}
+		else if(text=="1")
+		{
+			return 1;
+		}
+		else
+			throw new StringCalculatorException();
+	}
+
+
+	private static String[] splitNumbers(String numbers){
+	    return numbers.split(",");
+	}
+      
+    private static int sum(String[] numbers) throws StringCalculatorException{
+ 	    int total = 0;
+        for(String number : numbers){
+        	int temp;
+        	try {
+        		temp = Integer.parseInt(number);
+        	}
+        	catch (NumberFormatException e) {
+        		throw new StringCalculatorException();
+        	}
+		    total += temp;
+		}
+		return total;
+    }
+
 }
+
+
--- tests/StringCalculatorTest.java
+++ tests/StringCalculatorTest.java
@@ -5,8 +5,37 @@ import org.junit.Test;
 public class StringCalculatorTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testEmptyString() throws StringCalculatorException {
+		assertEquals(0, StringCalculator.Add(""));
 	}
 
+	@Test
+	public void testOneNumber() throws StringCalculatorException {
+		assertEquals(1, StringCalculator.Add("1"));
+	}
+
+	@Test
+	public void testTwoNumbers() throws StringCalculatorException {
+		assertEquals(3, StringCalculator.Add("1,2"));
+	}	
+
+	@Test
+    public void testMultipleNumbers() throws StringCalculatorException {
+    	assertEquals(10, StringCalculator.Add("1,2,7"));
+    }
+	
+	@Test
+    public void testMultipleNumberswithn() throws StringCalculatorException {
+    	assertEquals(6, StringCalculator.Add("1\n2,3"));
+    }
+
+	@Test (expected = StringCalculatorException.class)
+    public void testNegNumber() throws StringCalculatorException {
+    	assertEquals(6, StringCalculator.Add("-1,2,3"));
+    }
+	
+	@Test (expected = StringCalculatorException.class)
+    public void testinvalidinput() throws StringCalculatorException {
+    	assertEquals(6, StringCalculator.Add("1,ss,tt"));
+    }
 }
