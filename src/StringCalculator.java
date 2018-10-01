d4ad6c77addb4bc611e62d1fde5443ced1e1a622
Kalle Ollonqvist <ollonqistkalle@gmail.com>
2018-10-01 14:33:38 +0300

    Assignment Finished

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,9 +1,27 @@
 
 public class StringCalculator {
-	public int add(String numbersStr) {
-		// Returns the sum of the numbers given in numbersStr
-		
-		// not yet implemented
-		return 0;
+	public int add(String numbersStr) throws StringCalculatorException {
+		if (numbersStr.length() > 1 && numbersStr.length() < 4){
+			String[] list = numbersStr.split(",|\\\n");
+			int num1 = Integer.parseInt(list[0]);
+			int num2 = Integer.parseInt(list[1]);
+			return(num1 + num2);
+		}
+		if (numbersStr.length() > 3) {
+			int result = addMultiple(numbersStr);
+			return result;
+		}
+
+		else return 0;
+	}
+	
+	private int addMultiple(String numbersStr) {
+		String[] list = numbersStr.split(",|\\\n");
+		int result = 0;
+		//System.out.println(list.length);
+		for (int i=0; list.length > i ; i++) {
+			result = result + Integer.parseInt(list[i]);
+		}
+		return result;
 	}
 }
--- tests/StringCalculatorTest.java
+++ tests/StringCalculatorTest.java
@@ -1,12 +1,55 @@
 import static org.junit.Assert.*;
 
 import org.junit.Test;
-
 public class StringCalculatorTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	@Test //USER STORY 1, REQUIREMENT 1
+	public void test_twoNumbers() throws StringCalculatorException {
+		StringCalculator calc = new StringCalculator();
+		int result = 0;
+		result = calc.add("1,2");
+		
+		assertEquals("3", 3, result);
 	}
+	
+	@Test //USER STORY 2, REQUIREMENT 1
+	public void test_oneNumber() throws StringCalculatorException {
+		StringCalculator calc = new StringCalculator();
+		int result = calc.add("1");
+		
+		assertEquals("0", 0, result);
+	}
+	
+	@Test //USER STORY 3, REQUIREMENT 2
+	public void test_multiple() throws StringCalculatorException {
+		StringCalculator calc = new StringCalculator();
+		int result = calc.add("1,2,3");
+		
+		assertEquals("6", 6, result);
+	}
+	
+	@Test //USER STORY 4, REQUIREMENT 3
+	public void test_twoNumbers_with_linebreak() throws StringCalculatorException {
+		StringCalculator calc = new StringCalculator();
+		int result = calc.add("1\n2");
+		
+		assertEquals("3", 3, result);
+	}
+	
+	@Test //USER STORY 5, REQUIREMENT 3
+	public void test_multiple_with_linebreak() throws StringCalculatorException {
+		StringCalculator calc = new StringCalculator();
+		int result = calc.add("1,2\n3");
+		
+		assertEquals("6", 6, result);
+	}
+	
 
+	@Test (expected = StringCalculatorException.class) //USER STORY 6, REQUIREMENT 4
+	public void test_twoNumbers_negative() throws StringCalculatorException {
+		StringCalculator calc = new StringCalculator();
+		int result = 0;
+		result = calc.add("1,-2");
+		
+	}
 }
