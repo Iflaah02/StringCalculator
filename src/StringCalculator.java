92f4b586e0b4c3628c0b8bfb9b09046ba09391e0
Kiree <petteri.salonurmi@hotmail.com>
2018-10-01 12:26:25 +0300

    [ASSIGNMENT FINISHED] :white_check_mark: Implemented the add method to StringCalculator.java :clipboard: Added unit tests for the StringCalculator.java :x: Added more ignored files to the .gitignore

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,9 +1,46 @@
+import java.util.Arrays;
+import java.util.List;
 
 public class StringCalculator {
-	public int add(String numbersStr) {
-		// Returns the sum of the numbers given in numbersStr
-		
-		// not yet implemented
-		return 0;
+
+	/**
+	 * add(String numbersStr)
+	 *
+	 * Adds the given numbers together.
+	 *
+	 * @param numbersStr - The numbers in a string
+	 * @return The sum of the numbers
+	 */
+	public int add(String numbersStr) throws StringCalculatorException {
+
+		final int[] sum = {0};
+		String regex = "[0-9, /,]+";
+
+		// Check that the string is not null or empty
+		if (numbersStr != null && !numbersStr.isEmpty()) {
+
+			// Replace whitespace with comma
+			numbersStr = numbersStr.replaceAll("\\s", ",");
+
+			// Abort execution if string contains characters other than numbers [0-9] or comma [/,].
+			if (!numbersStr.matches(regex)) {
+				throw new StringCalculatorException();
+			}
+
+			List<String> numbersList = Arrays.asList(numbersStr.split(",")); // Assign the expected numbers to an array
+
+			try {
+				numbersList.forEach((currentItem) -> {
+					if(!currentItem.isEmpty()) {
+						sum[0] = sum[0] + Integer.parseInt(currentItem);
+					}
+				});
+				return sum[0]; // Return the sum of the items
+			} catch (NumberFormatException e) {
+				throw new StringCalculatorException(); // Throw an error on invalid characters.
+			}
+		} else {
+			return sum[0]; // Return 0 for null or empty string
+		}
 	}
 }
