c0f5a11fc8489fce68af97d3a1979b4f14385e90
Saku Ruohola <saku.ruohola@gmail.com>
2018-10-01 18:12:28 +0300

    Assignment Finished

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,9 +1,54 @@
 
 public class StringCalculator {
-	public int add(String numbersStr) {
+	
+	
+	public int add(String numbersStr) throws StringCalculatorException  {
 		// Returns the sum of the numbers given in numbersStr
+		int sum = 0;
+		int[] numberArray = new int[3];
 		
-		// not yet implemented
-		return 0;
+		if(!numbersStr.isEmpty()) {
+			
+			numberArray = splitInput(numbersStr);
+			for(int i = 0; i < numberArray.length; i++) {
+				if(numberArray[i] >= 0) {
+					testOfValue(numberArray[i]);
+					sum += numberArray[i];
+				} else throw new StringCalculatorException();
+			}
+		}
+		
+
+		return sum;
 	}
+	
+	private int[] splitInput(String numbersToSplit) {
+		String [] numbers;
+		int[] sum = new int[3];
+		numbers = numbersToSplit.split("[,|\n]");
+		if(testOfLength(numbers))		{
+			for(int i = 0; i < numbers.length; i++) {				
+					sum[i] = Integer.parseInt(numbers[i]);				
+			}
+		}
+		
+		return sum; 
+	}
+	
+	private boolean testOfLength(String[] testString) {
+		boolean test;
+		if(testString.length >= 0 && testString.length < 3) {
+			test = true;
+		} else test = false;
+		return test;
+	}
+	
+	private boolean testOfValue(int value) throws StringCalculatorException {
+		boolean test;
+		if(value < 0) {
+			 test = true;
+		} else throw new StringCalculatorException();
+		return test;
+	}
+
 }
