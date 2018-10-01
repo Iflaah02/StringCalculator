4cae56665025ef20af7c829532c5296a0dbc2cab
Pepetti <pirkkapekka@gmail.com>
2018-10-01 21:58:50 +0300

    Assignment Finished

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,9 +1,59 @@
+import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class StringCalculator {
-	public int add(String numbersStr) {
-		// Returns the sum of the numbers given in numbersStr
-		
-		// not yet implemented
-		return 0;
+	
+	public int add(String numbersStr) throws StringCalculatorException {
+		String[] strArray = toStringArray(numbersStr);
+		int[] intArray = toIntArray(strArray);
+		int sum = IntStream.of(intArray).sum();
+		return sum;
+	}
+	
+	public String[] toStringArray (String numbersStr) throws StringCalculatorException {
+		String text1 = handleUnknownNumbers(numbersStr);
+		String text2 = handleEmptyString(text1);
+		String text3 = handleLineBreakAndComma(text2);
+		String text4 = handleAlphabeticAndNegative(text3);
+		String[] split = text4.split(",");
+		return split;
+	}
+	
+	public int[] toIntArray (String[] arr) {
+		int[] ints = new int [arr.length];
+		for (int i = 0; i < arr.length; i++) {
+			ints[i] = Integer.parseInt(arr[i]);
+		}
+		return ints;
+	}
+	
+	public String handleLineBreakAndComma (String numbersStr) throws StringCalculatorException{
+		if (numbersStr.contains(",\n")) {
+			throw new StringCalculatorException();
+		}else{
+		String text = numbersStr.replace("\n", ",");
+		return text;
+		}
+	}	
+	public String handleEmptyString (String numbersStr) {
+		if (numbersStr == "0") {
+			return "";
+		}else { 
+			return numbersStr;
+		}
+	}
+	public String handleUnknownNumbers(String numbersStr) throws StringCalculatorException  {
+		if (numbersStr.length() > 3 && numbersStr.contains("\n") == false) {
+			throw new StringCalculatorException();
+		}else {
+			return numbersStr;
+		}
+	}
+	public String handleAlphabeticAndNegative(String numbersStr) throws StringCalculatorException {
+		if (numbersStr.matches("[0-9,]+") == false) {
+			throw new StringCalculatorException();
+		}else {
+			return numbersStr;
+		}
 	}
 }
