87dcb74f6bd315e63384eb8eba1c94dbc238f1d9
Kalle Vuorinen <kalle.vuorinen@suprakraft.com>
2018-10-02 21:46:05 +0300

    Assignment finished

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,9 +1,58 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/*
+Implement a simple String calculator with a method int Add(string numbers)
+1. The method can take 0, 1 or 2 numbers, and will return their sum (for an empty string it will
+return 0) for example “” or “1” or “1,2”
+2. Add method to handle an unknown amount of numbers
+3. Add method to handle new lines between numbers (instead of commas).
+1. the following input is ok: “1\n2,3” (will equal 6)
+2. the following input is NOT ok: “1,\n”
+4. Calling Add with a negative numbers or invalid input (not numbers) will throw an
+StringCalculatorException
+*/
 
 public class StringCalculator {
-	public int add(String numbersStr) {
-		// Returns the sum of the numbers given in numbersStr
+	// Main function
+	public int add(String numbersStr) throws StringCalculatorException {
 		
-		// not yet implemented
-		return 0;
+		int val = 0;
+		if (numbersStr != null && numbersStr != "") {			
+			try {
+				// Remove the "" from the list
+				String[] numbersArray = convertToNumberStringArray(numbersStr);
+				numbersArray = removeEmptyStrings(numbersArray);
+			
+				for (String numberString : numbersArray) {
+					val += Integer.parseInt(numberString);
+				}
+			}
+			catch (IllegalArgumentException e) {
+				throw new StringCalculatorException();
+			}
+		}
+		return val;
+	}
+	
+	// Beautify this class and handle errors
+	public String[] convertToNumberStringArray(String numbers) {
+		
+		String[] arrayOfNumbers = numbers.split("\\s*,\\s*"); 
+	
+		return arrayOfNumbers;
+	}
+	
+	public String[] removeEmptyStrings(String[] numbersArray) {
+		
+		List<String> myList = new ArrayList<String>(Arrays.asList(numbersArray));
+		myList.remove("");
+		
+		String[] myStrArray = myList.toArray(new String[0]);
+		
+		return myStrArray;
 	}
 }
--- tests/StringCalculatorTest.java
+++ tests/StringCalculatorTest.java
@@ -3,10 +3,124 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class StringCalculatorTest {
-
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test_add_valid() throws StringCalculatorException{
+		// Arrange
+		StringCalculator calculator = new StringCalculator();
+		String testString = "11, 2, , 3, 31";
+		
+		// Act
+		int returnedValue = calculator.add(testString);
+		
+		// Assert
+		assertEquals(47, returnedValue);
 	}
 
+	@Test
+	public void test_add_with_zero() throws StringCalculatorException{
+		// Arrange			
+		StringCalculator calculator = new StringCalculator();
+		String testString = "0, 2, , 3";
+		
+		// Act
+		int returnedValue = calculator.add(testString);
+		
+		// Assert
+		assertEquals(5, returnedValue);
+	}
+	
+	@Test
+	public void test_add_zero() throws StringCalculatorException{
+		// Arrange	
+		StringCalculator calculator = new StringCalculator();
+		String testString = "0, ";
+		
+		// Act
+		int returnedValue = calculator.add(testString);
+		
+		// Assert
+		assertEquals(0, returnedValue);
+	}
+
+	@Test  (expected = StringCalculatorException.class)
+	public void test_add_empty() throws StringCalculatorException{
+		// Arrange	
+		StringCalculator calculator = new StringCalculator();
+		String testString = " ";
+		
+		// Act
+		int returnedValue = calculator.add(testString);
+	}
+
+	@Test
+	public void test_add_null() throws StringCalculatorException{
+		// Arrange	
+		StringCalculator calculator = new StringCalculator();
+		
+		// Act
+		int returnedValue = calculator.add(null);
+		
+		// Assert
+		assertEquals(0, returnedValue);
+	}
+	
+	@Test
+	public void test_removeEmptyString() {
+		// Arrange
+		StringCalculator calculator = new StringCalculator();
+		String[] testArray = {"21", "1", "", "15"};
+		
+		// Act
+		String[] returnedArray = calculator.removeEmptyStrings(testArray);
+		
+		// Assert
+		String[] expectedArray = {"21", "1", "15"};
+		assertArrayEquals(returnedArray, expectedArray);
+	}
+	
+	@Test
+	public void test_convertToArray() {
+		// Arrange
+		StringCalculator calculator = new StringCalculator();
+		String testString = "1,15\n , ,3";
+		
+		// Act
+		String[] returnedArray = calculator.convertToNumberStringArray(testString);
+		
+		// Assert
+		String[] expectedArray = {"1", "15", "", "3"};
+		assertArrayEquals(returnedArray, expectedArray);
+	}
+	
+	
+	@Test 
+	public void test_add_plainString() throws StringCalculatorException{
+		// Arrange
+		StringCalculator calculator = new StringCalculator();
+		String testString = "1,40\n, 3";
+		
+		// Act
+		int returnedValue = calculator.add(testString);
+		
+		// Assert
+		int expected = 44;
+		assertEquals(expected, returnedValue);
+		
+	}
+	
+	@Test 
+	public void test_add_plainStringWithEmpty() throws StringCalculatorException{
+		// Arrange
+		StringCalculator calculator = new StringCalculator();
+		String testString = " , 2\n , , , , ";
+		
+		// Act
+		int returnedValue = calculator.add(testString);
+		
+		// Assert
+		int expected = 2;
+		assertEquals(expected, returnedValue);
+		
+	}
 }
